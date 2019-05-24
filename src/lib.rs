755d83e5918b65a8e712f6906a6484d797b71c9c
clux <sszynrae@gmail.com>
2019-05-24 22:15:24 +0100

    Remove unused library

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 #[macro_use] extern crate failure;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate log;
-#[macro_use] extern crate lazy_static;
 
 extern crate base64;
 extern crate dirs;
