a3c48718340d293173529daedd0c3d0711b8463c
clux <sszynrae@gmail.com>
2019-08-11 19:36:20 +0100

    try azure pipelines - for #62

---
+++ ci/clippy.sh
@@ -0,0 +1,22 @@
+set -euo pipefail
+
+if [ ! -d ~/.cargo/bin ]; then
+	mkdir -p ~/.cargo/bin
+	curl -Lo ~/.cargo/bin/rustup 'https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init'
+	chmod +x ~/.cargo/bin/rustup
+	ln -s ~/.cargo/bin/rustup ~/.cargo/bin/cargo
+	ln -s ~/.cargo/bin/rustup ~/.cargo/bin/cargo-clippy
+	ln -s ~/.cargo/bin/rustup ~/.cargo/bin/rustc
+	ln -s ~/.cargo/bin/rustup ~/.cargo/bin/rustdoc
+	export PATH="$PATH:$(realpath ~/.cargo/bin)"
+fi
+
+rustup install stable
+rustup default stable
+
+rustup component add clippy
+
+# Saves a few seconds for large crates
+export CARGO_INCREMENTAL=0
+
+cargo clippy --all-features -- -D warnings
