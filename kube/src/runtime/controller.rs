cc03b892af88f39514704e69bd66965b7d61cb21
clux <sszynrae@gmail.com>
2020-03-09 01:17:51 +0000

    ideas on how to do controller loop

--- kube/src/runtime/controller.rs
+++ kube/src/runtime/controller.rs
@@ -1,13 +1,17 @@
+use futures::lock::Mutex;
+use std::sync::Arc;
+use std::collections::VecDeque;
 use crate::{
     api::{
         resource::{ListParams, Resource},
         Meta,
+        WatchEvent
     },
     client::APIClient,
     runtime::informer::Informer,
     Result,
 };
-use futures::{stream, Stream};
+use futures::{stream, Stream, StreamExt};
 use serde::de::DeserializeOwned;
 
 #[derive(Clone)]
@@ -18,8 +22,10 @@ where
     client: APIClient,
     resource: Resource,
     watches: Vec<Informer<K>>,
+    queue: Arc<Mutex<VecDeque<ReconcileEvent>>>,
 }
 
+#[derive(Debug)]
 pub struct ReconcileEvent {
     pub name: String,
     pub namespace: Option<String>,
@@ -27,7 +33,7 @@ pub struct ReconcileEvent {
 
 impl<K> Controller<K>
 where
-    K: Clone + DeserializeOwned + Meta,
+    K: Clone + DeserializeOwned + Meta + Send + Sync,
 {
     /// Create a controller with a kube client on a kube resource
     pub fn new(client: APIClient, r: Resource) -> Self {
@@ -35,6 +41,7 @@ where
             client: client,
             resource: r,
             watches: vec![],
+            queue: Default::default(),
         }
     }
 
@@ -48,17 +55,35 @@ where
 
     /// Poll reconcile events through all internal informers
     pub async fn poll(&self) -> Result<impl Stream<Item = Result<ReconcileEvent>>> {
-        // TODO: poll informers in parallel, have them push to a joint queue
-        // TODO: debounce and read from joint queue
-        // TODO: pass on debounced events to stream
-        Ok(stream::empty())
+        // 1. poll informers in parallel and push results to queue
+        for inf in self.watches {
+            // TODO: init all watchers and link them up;
+            let queue = self.queue.clone();
+            tokio::spawn(async move {
+                let mut poll_i = inf.poll().await.unwrap().boxed();
+                while let Some(ev) = poll_i.next().await {
+                    // TODO: add_permit to a tokio semaphore here..
+                    match ev {
+                        Ok(WatchEvent::Added(o)) => {
+                            (*queue.lock().await).push_back(ReconcileEvent {
+                                name: Meta::name(&o),
+                                namespace: Meta::namespace(&o)
+                            });
+                        }
+                        _ => unimplemented!()
+                    }
+                }
+            });
+        }
+        // TODO: debounce rx events
+        // read from queue here with semaphore.acquire
+        Ok(rx)
     }
 
     /// Initialize
     pub fn init(self) -> Self {
         info!("Starting Controller for {:?}", self.resource);
         // TODO: init main informer
-        // TODO: init all watchers and link them up
         // TODO: queue up events
         // TODO: debounce events
         // TODO: trigger events
