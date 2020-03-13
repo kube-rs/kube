80a6d99fa2570baec4f23308caba256e072e2e4a
clux <sszynrae@gmail.com>
2020-03-13 18:54:26 +0000

    release tweaks

--- release.sh
+++ release.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 
+fail() {
+  echo "$@"
+  exit 2
+}
+
 git-tag() {
-  git diff --exit-code || return 1
-  git diff --cached --exit-code || return 1
-  # TODO: check no stray files (cargo publish dislikes it)
+  [ -z "$(git ls-files . --exclude-standard --others)" ] || fail "remove untracked files first"
+  git commit -am "${VERSION}"
   git tag -a "${VERSION}" -m "${VERSION}"
   git push
   git push --tags
+}
+
+publish-changed-crate() {
+  local -r crate="$1"
+  if git diff --name-only origin/master "${crate}/Cargo.toml"; then
+    (cd "$crate"; cargo publish)
+  fi
+}
+
+publish() {
+  git-tag
+  publish-changed-crate kube
+  publish-changed-crate kube-derive
 }
 
 bump-files() {
@@ -21,46 +38,40 @@ main() {
   local -r VERSION="$(grep version kube/Cargo.toml | awk -F"\"" '{print $2}' | head -n 1)"
   local -r SEMVER=(${VERSION//./ }) # <- parse subset of semver here
   # NB: can maybe use cargo-bump in the future  if it starts supporting workspaces
+  local NEWVER="" # set if we are bumping
 
   local -r mode="$1"
   if [[ "${mode}" == "major" ]]; then
-    local -r NEWVER="$((SEMVER[0]+1)).${SEMVER[1]}.${SEMVER[2]}"
+    NEWVER="$((SEMVER[0]+1)).${SEMVER[1]}.${SEMVER[2]}"
   elif [[ "${mode}" == "minor" ]]; then
-    local -r NEWVER="${SEMVER[0]}.$((SEMVER[1]+1)).${SEMVER[2]}"
+    NEWVER="${SEMVER[0]}.$((SEMVER[1]+1)).${SEMVER[2]}"
   elif [[ "${mode}" == "patch" ]]; then
-    local -r NEWVER="${SEMVER[0]}.${SEMVER[1]}.$((SEMVER[2]+1))"
+    NEWVER="${SEMVER[0]}.${SEMVER[1]}.$((SEMVER[2]+1))"
   fi
 
   # bumping something:
-  if [[ "${VERSION}" != "${NEWVER}" ]]; then
+  if [ -n "${NEWVER}" ]; then
+    [ -z "$(git status --porcelain)" ] || fail "deal with changes first"
     echo "Bumping from ${VERSION} -> ${NEWVER}"
     bump-files
     git diff
     exit 0
   fi
 
-  # release
-  if [[ "${mode}" == "release" ]]; then
-    if ! git diff --name-only origin/master ./*/Cargo.toml; then
-      echo "Please run \"./release.sh minor\" first"
-      exit 2
-    fi
-    git commit -am "${VERSION}"
-    git-tag
-    echo "Please commit and run script with tag mode"
-    if git diff --name-only --origin/master kube/Cargo.toml; then
-      echo "Please cargo publish inside kube/"
-    fi
-    if git diff --name-only --origin/master kube-derive/Cargo.toml; then
-      echo "Please cargo publish inside kube-derive/"
-    fi
+  # publish
+  if [[ "${mode}" == "publish" ]]; then
+    [ -n "$(git diff --name-only origin/master ./*/Cargo.toml)" ] || fail "./release.sh minor" first
+    publish
   fi
 }
 
-# Usage: 3 stage (for now)
+# Usage: 2 stage
+#
 # ./release.sh minor
-# ./release.sh release
-# cargo publish (in required subdirectories)
-
+#
+# Then check git output, and:
+#
+# ./release.sh publish
+#
 # shellcheck disable=SC2068
 main $@
