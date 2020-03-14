65f249399b168bbf9575aaae1fcd8cfb0ca8cea6
clux <sszynrae@gmail.com>
2020-03-14 15:08:23 +0000

    make an event channel

--- kube/src/runtime/controller.rs
+++ kube/src/runtime/controller.rs
@@ -1,37 +1,71 @@
-use futures::lock::Mutex;
-use std::sync::Arc;
-use std::collections::VecDeque;
 use crate::{
     api::{
         resource::{ListParams, Resource},
-        Meta,
-        WatchEvent
+        Meta, WatchEvent,
     },
     client::APIClient,
     runtime::informer::Informer,
-    Result,
+    Error, Result,
 };
-use futures::{stream, Stream, StreamExt};
+use futures::{channel::mpsc, lock::Mutex, stream, Stream, StreamExt};
 use serde::de::DeserializeOwned;
+use std::{collections::VecDeque, convert::TryFrom, sync::Arc};
 
-#[derive(Clone)]
+/// An object to be reconciled
+///
+/// The type that is pulled out of Controller::poll
+#[derive(Debug, Clone)]
+pub struct ReconcileEvent {
+    pub name: String,
+    pub namespace: Option<String>,
+}
+
+impl<K> From<K> for ReconcileEvent
+where
+    K: Meta,
+{
+    fn from(k: K) -> ReconcileEvent {
+        ReconcileEvent {
+            name: Meta::name(&k),
+            namespace: Meta::namespace(&k),
+        }
+    }
+}
+
+impl<K> TryFrom<WatchEvent<K>> for ReconcileEvent
+where
+    K: Meta + Clone,
+{
+    type Error = crate::Error;
+
+    /// Helper to convert the openapi ReplicaSet to the useful info
+    fn try_from(w: WatchEvent<K>) -> Result<ReconcileEvent> {
+        match w {
+            WatchEvent::Added(o) => Ok(o.into()),
+            WatchEvent::Modified(o) => Ok(o.into()),
+            WatchEvent::Deleted(o) => Ok(o.into()),
+            WatchEvent::Error(e) => Err(Error::Api(e)),
+        }
+    }
+}
+
+/// A controller for a kubernetes object K
 pub struct Controller<K>
 where
     K: Clone + DeserializeOwned + Meta,
 {
     client: APIClient,
     resource: Resource,
-    watches: Vec<Informer<K>>,
+    informers: Vec<Informer<K>>,
     queue: Arc<Mutex<VecDeque<ReconcileEvent>>>,
+    channel: (
+        mpsc::UnboundedSender<Result<ReconcileEvent>>,
+        mpsc::UnboundedReceiver<Result<ReconcileEvent>>,
+    ),
 }
 
-#[derive(Debug)]
-pub struct ReconcileEvent {
-    pub name: String,
-    pub namespace: Option<String>,
-}
 
-impl<K> Controller<K>
+impl<K: 'static> Controller<K>
 where
     K: Clone + DeserializeOwned + Meta + Send + Sync,
 {
@@ -40,8 +74,9 @@ where
         Controller {
             client: client,
             resource: r,
-            watches: vec![],
+            informers: vec![],
             queue: Default::default(),
+            channel: mpsc::unbounded(),
         }
     }
 
@@ -49,40 +84,42 @@ where
     ///
     /// TODO: this needs to only find resources with a property matching root resource
     pub fn owns(mut self, r: Resource, lp: ListParams) -> Self {
-        self.watches.push(Informer::new(self.client.clone(), lp, r));
+        self.informers.push(Informer::new(self.client.clone(), lp, r));
         self
     }
 
     /// Poll reconcile events through all internal informers
-    pub async fn poll(&self) -> Result<impl Stream<Item = Result<ReconcileEvent>>> {
+    /*    pub async fn poll(&self) -> Result<impl Stream<Item = Result<ReconcileEvent>>> {
+            // TODO: debounce rx events
+            //let stream = stream::from(self.channel.1);
+                //futures::stream::try_unfold(self.channel.1, |rx| async move { async { return rx.try_next() } });
+            Ok(self.channel.1)
+        }
+    */
+    /// Initialize
+    pub fn init(self) -> Self {
+        info!("Starting Controller for {:?}", self.resource);
+
         // 1. poll informers in parallel and push results to queue
-        for inf in self.watches {
-            // TODO: init all watchers and link them up;
-            let queue = self.queue.clone();
+        for inf in self.informers.clone() {
+            // TODO: ownership move?
+            //let queue = self.queue.clone();
+            let tx = self.channel.0.clone();
             tokio::spawn(async move {
                 let mut poll_i = inf.poll().await.unwrap().boxed();
                 while let Some(ev) = poll_i.next().await {
-                    // TODO: add_permit to a tokio semaphore here..
                     match ev {
-                        Ok(WatchEvent::Added(o)) => {
-                            (*queue.lock().await).push_back(ReconcileEvent {
-                                name: Meta::name(&o),
-                                namespace: Meta::namespace(&o)
-                            });
+                        Ok(wi) => {
+                            let ri = ReconcileEvent::try_from(wi);
+                            //(*queue.lock().await).push_back(ri);
+                            tx.unbounded_send(ri).expect("channel can receive");
                         }
-                        _ => unimplemented!()
+                        _ => unimplemented!(),
+                        //Err(e) => tx.unbounded_send(Err(e)),
                     }
                 }
             });
         }
-        // TODO: debounce rx events
-        // read from queue here with semaphore.acquire
-        Ok(rx)
-    }
-
-    /// Initialize
-    pub fn init(self) -> Self {
-        info!("Starting Controller for {:?}", self.resource);
         // TODO: init main informer
         // TODO: queue up events
         // TODO: debounce events
