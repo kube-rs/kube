e29c768da73e9440e0b4958b52c39b621bbc697c
clux <sszynrae@gmail.com>
2020-03-22 21:45:48 +0000

    this error does not exist yet

--- kube/src/lib.rs
+++ kube/src/lib.rs
@@ -37,10 +37,6 @@ pub enum Error {
     #[error("Error deserializing response")]
     SerdeError(#[from] serde_json::Error),
 
-    /// Common error when trying to serialize arbitrary app yaml
-    #[error("Error serializing request")]
-    SerdeYamlError(#[from] serde_yaml::Error),
-
     #[error("Error building request")]
     RequestBuild,
     #[error("Error executing request")]
