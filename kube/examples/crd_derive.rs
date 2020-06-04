17cd8d0b16da82c1475e554c99228a1763bf0d19
clux <sszynrae@gmail.com>
2020-06-04 14:26:22 +0100

    pluralised finalizer name seems better practice to match kube url

--- kube/examples/crd_derive.rs
+++ kube/examples/crd_derive.rs
@@ -12,7 +12,7 @@ use serde::{Deserialize, Serialize};
     kind = "Foo",
     namespaced,
     status = "FooStatus",
-    finalizer = "mygc.foo.clux.dev",
+    finalizer = "mygc.foos.clux.dev",
     scale = r#"{"specReplicasPath":".spec.replicas", "statusReplicasPath":".status.replicas"}"#,
     printcolumn = r#"{"name":"Spec", "type":"string", "description":"name of foo", "jsonPath":".spec.name"}"#
 )]
@@ -36,7 +36,7 @@ fn main() {
     foo.status = Some(FooStatus { is_bad: true });
     println!("Spec: {:?}", foo.spec);
     println!("finalizers: {:?}", foo.metadata.finalizers);
-    println!("Foo CRD: {:?}", Foo::crd());
+    println!("Foo CRD: \n{}", serde_json::to_string_pretty(&Foo::crd()).unwrap());
 }
 
 // some tests
