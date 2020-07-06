be56f707f913c50ab3e4fe8c57b388e8eaf9812a
clux <sszynrae@gmail.com>
2020-07-06 21:58:27 +0100

    rewrite an example to using new reflector
    currently lacking state() from the old api.
    though while this was super expensive before, introducing it now would
      expose dashmap as interface, which isn't great.
    
    maybe we can do something clever here, return an iter / weak ptr. also
    not entirely sure why we are using dashmap.

--- kube/examples/deployment_reflector.rs
+++ kube/examples/deployment_reflector.rs
@@ -1,10 +1,11 @@
 #[macro_use] extern crate log;
+use futures::TryStreamExt;
 use k8s_openapi::api::apps::v1::Deployment;
 use kube::{
     api::{Api, ListParams, Meta},
-    runtime::Reflector,
     Client,
 };
+use kube_runtime::{reflector, reflector::{ObjectRef}, watcher, utils::try_flatten_applied,};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -14,19 +15,38 @@ async fn main() -> anyhow::Result<()> {
 
     let namespace = std::env::var("NAMESPACE").unwrap_or("default".into());
 
-    let deploys: Api<Deployment> = Api::namespaced(client, &namespace);
-    let lp = ListParams::default().timeout(10); // short watch timeout in this example
-    let rf = Reflector::new(deploys).params(lp);
+    let store = reflector::store::Writer::<Deployment>::default();
+    let reader = store.as_reader();
+    let rf = reflector(
+        store,
+        watcher(
+            Api::<Deployment>::namespaced(client.clone(), &namespace),
+            ListParams::default().timeout(10) // short watch timeout in this example
+        )
+    );
 
-    let rf2 = rf.clone(); // read from a clone in a task
+    // We can also interact with state in another thread
+    let pref = ObjectRef::new_namespaced("prometheus-operator".to_string(), namespace.clone());
+    // FIXME: ^ this is an awkward interface
     tokio::spawn(async move {
         loop {
             // Periodically read our state
             tokio::time::delay_for(std::time::Duration::from_secs(5)).await;
-            let deploys: Vec<_> = rf2.state().await.unwrap().iter().map(Meta::name).collect();
-            info!("Current deploys: {:?}", deploys);
+            let prom = reader.get(&pref);
+            info!("prometheus?: {:?}", prom);
+            //let deploys: Vec<_> = reader.state().iter().map(|k| Meta::name(&k)).collect();
+            //info!("Current deploys: {:?}", deploys);
+            // FIXME: full state interface / weak ptr?
+            // currently a full fetch feels limited by dashmap, don't want to expose that interface
+            // also not sure what the benefit of it is atm..
         }
     });
-    rf.run().await?; // run reflector and listen for signals
+
+    // We can look at the events we want and use it as a watcher
+    let mut rfa = Box::pin(try_flatten_applied(rf));
+    while let Some(event) = rfa.try_next().await? {
+        info!("Applied {:?}", event);
+    }
+
     Ok(())
 }
