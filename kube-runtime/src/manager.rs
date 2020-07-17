ed7c4924a88b903caf6cee3904ddf0293be10279
clux <sszynrae@gmail.com>
2020-07-17 13:10:31 +0100

    comments

--- kube-runtime/src/manager.rs
+++ kube-runtime/src/manager.rs
@@ -11,18 +11,15 @@ use crate::{
 use futures::{stream, Stream, TryFuture, TryStream};
 use kube::api::{Api, ListParams, Meta};
 use serde::de::DeserializeOwned;
-
 use std::pin::Pin;
 
 /// A builder for controller
 pub struct ControllerBuilder<K>
 where
     K: Clone + Meta + 'static,
-    //E: std::error::Error + 'static,
-    //QueueStream: TryStream<Ok = ObjectRef<K>>,
-    //QueueStream::Error: std::error::Error + 'static,
 {
     // NB: Need to Unpin for stream::select_all
+    // TODO: get an arbitrary std::error::Error in here?
     selector: Vec<Pin<Box<dyn Stream<Item = Result<ObjectRef<K>, watcher::Error>>>>>,
     reader: Store<K>,
 }
@@ -30,11 +27,8 @@ where
 impl<K> ControllerBuilder<K>
 where
     K: Clone + Meta + DeserializeOwned + 'static,
-    //E: std::error::Error + 'static,
-    //QueueStream: TryStream<Ok = ObjectRef<K>>,
-    //QueueStream::Error: std::error::Error + 'static
 {
-    fn new(owned_api: Api<K>, writer: Writer<K>, lp: ListParams) -> Self {
+    pub fn new(owned_api: Api<K>, writer: Writer<K>, lp: ListParams) -> Self {
         let reader = writer.as_reader();
         let mut selector = vec![];
         let self_watcher: Pin<Box<dyn Stream<Item = Result<ObjectRef<K>, watcher::Error>>>> = Box::pin(
@@ -44,7 +38,7 @@ where
         Self { selector, reader }
     }
 
-    fn owns<KOwns: Clone + Meta + DeserializeOwned + 'static>(
+    pub fn owns<KOwns: Clone + Meta + DeserializeOwned + 'static>(
         mut self,
         api: Api<KOwns>,
         lp: ListParams,
@@ -57,7 +51,7 @@ where
     // TODO: fn watches and arbitrary stream?
 
     /// Consume the ControllerBuilder and create the controller
-    fn run<ReconcilerFut, T, QueueStream>(
+    pub fn run<ReconcilerFut, T, QueueStream>(
         self,
         reconciler: impl FnMut(K, Context<T>) -> ReconcilerFut,
         error_policy: impl FnMut(&ReconcilerFut::Error, Context<T>) -> ReconcilerAction,
@@ -70,7 +64,6 @@ where
         QueueStream: TryStream<Ok = ObjectRef<K>>,
         QueueStream::Error: std::error::Error + 'static,
     {
-        // TODO: how to get Items in this stream to have the same opaque type?
         let input_stream = stream::select_all(self.selector);
         controller(reconciler, error_policy, context, self.reader, input_stream)
     }
