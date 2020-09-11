c88500d47dfb4e6fc5f0a6eec7ad15e14fa3d853
clux <sszynrae@gmail.com>
2020-09-11 13:17:23 +0100

    fix tests

--- examples/crd_api.rs
+++ examples/crd_api.rs
@@ -15,7 +15,7 @@ use kube::{
 
 // Own custom resource
 #[derive(CustomResource, Deserialize, Serialize, Clone, Debug)]
-#[kube(group = "clux.dev", version = "v1", namespaced)]
+#[kube(group = "clux.dev", version = "v1", kind = "Foo", namespaced)]
 #[kube(apiextensions = "v1beta1")]
 #[kube(status = "FooStatus")]
 #[kube(scale = r#"{"specReplicasPath":".spec.replicas", "statusReplicasPath":".status.replicas"}"#)]
