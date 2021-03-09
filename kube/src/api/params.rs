2302b03b65cc1d4c6a8c3007c6cbbcb824f80d2b
Lars Francke <lars.francke@gmail.com>
2021-03-09 13:20:51 +0100

    Add documentation for Json variant

--- kube/src/api/params.rs
+++ kube/src/api/params.rs
@@ -203,7 +203,27 @@ pub enum Patch<T: Serialize> {
     ///
     /// Requires kubernetes >= 1.16
     Apply(T),
+
     /// [JSON patch](https://kubernetes.io/docs/tasks/run-application/update-api-object-kubectl-patch/#use-a-json-merge-patch-to-update-a-deployment)
+    ///
+    /// Note: Using this variant might require you to explicitly provide a type for `T`.
+    ///       This is a quirk in the API.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use kube::api::Patch;
+    /// let json_patch = json_patch::Patch(vec![]);
+    /// let patch = Patch::Json::<()>(json_patch);
+    /// ```
+    ///
+    /// This will fail to compile:
+    ///
+    /// ```compile_fail
+    /// use kube::api::Patch;
+    /// let json_patch = json_patch::Patch(vec![]);
+    /// let patch = Patch::Json(json_patch);
+    /// ```
     #[cfg(feature = "jsonpatch")]
     #[cfg_attr(docsrs, doc(cfg(feature = "jsonpatch")))]
     Json(json_patch::Patch),
