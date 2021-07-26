e2f816e79ebd82b4dba0b6fdbde2f0cfad032913
Eirik A <sszynrae@gmail.com>
2021-07-26 19:20:56 +0100

    use cluster dns to reach apiserver for rustls - for #587 (#597)
    * use cluster dns to reach apiserver when on rustls - for #587
    
    * ci test run with rustls

--- kube/src/config/incluster_config.rs
+++ kube/src/config/incluster_config.rs
@@ -2,8 +2,12 @@ use std::env;
 
 use crate::{config::utils, Result};
 
+// Old method to connect to kubernetes
 pub const SERVICE_HOSTENV: &str = "KUBERNETES_SERVICE_HOST";
 pub const SERVICE_PORTENV: &str = "KUBERNETES_SERVICE_PORT";
+// New method to connect to kubernetes
+pub const SERVICE_DNS: &str = "kubernetes.default.svc";
+// Mounted credential files
 const SERVICE_TOKENFILE: &str = "/var/run/secrets/kubernetes.io/serviceaccount/token";
 const SERVICE_CERTFILE: &str = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt";
 const SERVICE_DEFAULT_NS: &str = "/var/run/secrets/kubernetes.io/serviceaccount/namespace";
@@ -13,6 +17,15 @@ pub fn kube_server() -> Option<String> {
     let host = kube_host()?;
     let port = kube_port()?;
     Some(format!("https://{}:{}", host, port))
+}
+
+pub fn kube_dns() -> http::Uri {
+    http::Uri::builder()
+        .scheme("https")
+        .authority(SERVICE_DNS)
+        .path_and_query("/")
+        .build()
+        .unwrap()
 }
 
 fn kube_host() -> Option<String> {
@@ -43,6 +56,7 @@ fn test_kube_host() {
     let expected = "fake.io";
     env::set_var(SERVICE_HOSTENV, expected);
     assert_eq!(kube_host().unwrap(), expected);
+    kube_dns(); // verify kube_dns always unwraps
 }
 
 #[test]
