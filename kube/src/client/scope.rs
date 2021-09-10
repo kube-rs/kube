175d4cc5c9332a67dfd7cef27e804bf514b3bc8e
Teo Klestrup Röijezon <teo@nullable.se>
2021-09-10 02:14:46 +0200

    WIP: Subresources

--- kube/src/client/scope.rs
+++ kube/src/client/scope.rs
@@ -1,13 +1,16 @@
 //! Scopes delimiting which objects an API call applies to
 
+use std::ops::Deref;
+
 use k8s_openapi::{ClusterResourceScope, NamespaceResourceScope};
-use kube_core::Resource;
+use kube_core::{subresource::Scale, DynamicObject, Resource};
 
 /// A scope for interacting with Kubernetes objects
 pub trait Scope {
     /// The namespace associated with the [`Scope`], if any
     fn namespace(&self) -> Option<&str>;
 }
+pub trait TopLevelScope: Scope {}
 
 /// Access all objects in the cluster (that the user has permission to access)
 #[derive(Clone, Debug)]
@@ -18,6 +21,11 @@ pub struct NamespaceScope {
     /// Namespace that access is limited to
     pub namespace: String,
 }
+#[derive(Clone, Debug)]
+pub struct SubresourceScope<Parent: TopLevelScope, Kind: Resource> {
+    pub parent: Parent,
+    pub dyn_type: Kind::DynamicType,
+}
 /// A [`Scope`] that is resolved at runtime
 ///
 /// NOTE: By using [`DynamicScope`] you opt out of Kube's ability to validate that the scope is valid for a given operation
@@ -27,6 +35,7 @@ pub enum DynamicScope {
     Cluster(ClusterScope),
     /// Access all objects in one namespace (that the user has permission to access)
     Namespace(NamespaceScope),
+    Subresource(Box<SubresourceScope<DynamicScope, DynamicObject>>),
 }
 
 impl Scope for ClusterScope {
@@ -34,9 +43,16 @@ impl Scope for ClusterScope {
         None
     }
 }
+impl TopLevelScope for ClusterScope {}
 impl Scope for NamespaceScope {
     fn namespace(&self) -> Option<&str> {
         Some(&self.namespace)
+    }
+}
+impl TopLevelScope for NamespaceScope {}
+impl<Parent: TopLevelScope, Kind: Resource> self::Scope for SubresourceScope<Parent, Kind> {
+    fn namespace(&self) -> Option<&str> {
+        self.parent.namespace()
     }
 }
 impl Scope for DynamicScope {
@@ -44,11 +60,13 @@ impl Scope for DynamicScope {
         self.inner().namespace()
     }
 }
+impl TopLevelScope for DynamicScope {}
 impl DynamicScope {
     fn inner(&self) -> &dyn Scope {
         match self {
             DynamicScope::Cluster(scope) => scope,
             DynamicScope::Namespace(scope) => scope,
+            DynamicScope::Subresource(scope) => scope.deref(),
         }
     }
 
@@ -62,14 +80,19 @@ impl DynamicScope {
     }
 }
 
+/// Scope where a [`Resource`]'s objects can be listed from
+pub trait ResourceScope<Kind>: Scope {
+    fn group(&self) -> &str;
+    fn version(&self) -> &str;
+    fn resource(&self) -> &str;
+}
+impl<Kind: k8s_openapi::Resource<Scope = NamespaceResourceScope>> ResourceScope<Kind> for NamespaceScope {}
+impl<Kind: k8s_openapi::Resource> ResourceScope<Kind> for ClusterScope {}
+impl ResourceScope<Scale> for SubresourceScope<NamespaceScope, k8s_openapi::api::core::v1::Pod> {}
+impl<Kind> ResourceScope<Kind> for DynamicScope {}
+
 /// Scope where a [`Resource`]'s objects can be read from or written to
-pub trait NativeScope<Kind>: Scope {}
+pub trait NativeScope<Kind>: ResourceScope<Kind> {}
 impl<Kind: k8s_openapi::Resource<Scope = NamespaceResourceScope>> NativeScope<Kind> for NamespaceScope {}
 impl<Kind: k8s_openapi::Resource<Scope = ClusterResourceScope>> NativeScope<Kind> for ClusterScope {}
 impl<Kind> NativeScope<Kind> for DynamicScope {}
-
-/// Scope where a [`Resource`]'s objects can be listed from
-pub trait ListScope<Kind>: Scope {}
-impl<Kind: k8s_openapi::Resource<Scope = NamespaceResourceScope>> ListScope<Kind> for NamespaceScope {}
-impl<Kind: k8s_openapi::Resource> ListScope<Kind> for ClusterScope {}
-impl<Kind> ListScope<Kind> for DynamicScope {}
