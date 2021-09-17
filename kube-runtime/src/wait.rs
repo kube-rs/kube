1c46a293b72845f5d68c0b936e8f97f043a061da
Teo Klestrup Röijezon <teo@nullable.se>
2021-09-17 04:06:34 +0200

    Implement support for delete-and-finalize on top of waiter
    Signed-off-by: Teo Klestrup Röijezon <teo@nullable.se>

--- kube-runtime/src/wait.rs
+++ kube-runtime/src/wait.rs
@@ -47,9 +47,60 @@ where
             watcher::Event::Restarted(objs) => Ok(objs.first()),
             watcher::Event::Applied(_) => Ok(None),
         };
-        let result = obj.map(&mut cond);
+        let result = obj.map(|obj| !cond(obj));
         async move { result }
     })
     .try_for_each(|_| async { Ok(()) })
     .await
+}
+
+pub mod conditions {
+    use kube::Resource;
+
+    pub fn is_deleted<K: Resource>(uid: &str) -> impl Fn(Option<&K>) -> bool + '_ {
+        move |obj: Option<&K>| {
+            obj.map_or(
+                // Object is not found, success!
+                true,
+                // Object is found, but a changed uid would mean that it was deleted and recreated
+                |obj| obj.meta().uid.as_deref() != Some(uid),
+            )
+        }
+    }
+}
+
+pub mod delete {
+    use kube::{api::DeleteParams, Api, Resource};
+    use serde::de::DeserializeOwned;
+    use snafu::{OptionExt, ResultExt, Snafu};
+    use std::fmt::Debug;
+
+    use super::{await_condition, conditions};
+
+    #[derive(Snafu, Debug)]
+    pub enum Error {
+        NoUid,
+        Delete { source: kube::Error },
+        Await { source: super::Error },
+    }
+
+    #[allow(clippy::module_name_repetitions, clippy::missing_errors_doc)]
+    pub async fn delete_and_finalize<K: Clone + Debug + Send + DeserializeOwned + Resource + 'static>(
+        api: Api<K>,
+        name: &str,
+        delete_params: &DeleteParams,
+    ) -> Result<(), Error> {
+        let deleted_obj_uid = api
+            .delete(name, delete_params)
+            .await
+            .context(Delete)?
+            .either(
+                |mut obj| obj.meta_mut().uid.take(),
+                |status| status.details.map(|details| details.uid),
+            )
+            .context(NoUid)?;
+        await_condition(api, name, conditions::is_deleted(&deleted_obj_uid))
+            .await
+            .context(Await)
+    }
 }
