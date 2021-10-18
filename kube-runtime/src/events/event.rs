kube-rs/kube

commit 14033ba1ac723d1be88ed921c69b00b39495f6d9

Add EventRecorder abstraction. (#653)
* Add EventRecorder abstraction.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Formatting.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Use proper error message

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Rename module to `events`.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Add missing `secondary_object` field.
Rename `instance_name` to `controller_pod_name`, including related structs.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Add notes detailing the name of the API field, for extra clarity.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Keep the Display representation free of details already included in the Debug representation.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Add pointers on how to get the controller pod name.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Collapse small modules.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Formatting.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Use secondary object in EventRecorder.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Add validation for event reason.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Add validation for event action.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Add validation for event note.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Formatting.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Fix note doc example.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Change field names.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

* Typo.

Signed-off-by: Luca Palmieri <lpalmieri@truelayer.com>

kube-runtime/src/events/event.rs
@@ -1,0 +1,346 @@
+use k8s_openapi::api::core::v1::ObjectReference;
+use std::{convert::TryFrom, fmt::Formatter};
+
+/// Required information to publish a new event via [`EventRecorder::publish`].
+///
+/// [`EventRecorder::publish`]: crate::events::EventRecorder::publish
+pub struct NewEvent {
+    /// The action that was taken (either successfully or unsuccessfully) against
+    /// the references object.
+    ///
+    /// `action` must be machine-readable.
+    pub action: EventAction,
+    /// The reason explaining why the `action` was taken.
+    ///
+    /// `reason` must be human-readable.
+    pub reason: EventReason,
+    /// A optional description of the status of the `action`.
+    ///
+    /// `note` must be human-readable.
+    pub note: Option<EventNote>,
+    /// The event severity.
+    pub event_type: EventType,
+    /// Some events are emitted for actions that affect multiple objects.
+    /// `secondary_object` can be populated to capture this detail.
+    ///
+    /// For example: the event concerns a `Deployment` and it
+    /// affects the current `ReplicaSet` underneath it.
+    /// You would therefore populate `secondary_object` using the object
+    /// reference of the `ReplicaSet`.
+    ///
+    /// Set `secondary_object` to `None`, instead, if the event
+    /// affects only the object whose reference you passed
+    /// to [`EventRecorder::new`].
+    ///
+    /// # Naming note
+    ///
+    /// `secondary_object` is mapped to `related` in
+    /// [`Events API`](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.22/#event-v1-events-k8s-io).
+    ///
+    /// [`EventRecorder::new`]: crate::events::EventRecorder::new
+    pub secondary_object: Option<ObjectReference>,
+}
+
+/// The event severity or type.
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+pub enum EventType {
+    /// An event took place - nothing to worry about.
+    Normal,
+    /// Something is not working as expected - it might be worth to have a look.
+    Warning,
+}
+
+/// Details about the event emitter.
+///
+/// ```rust
+/// use std::convert::TryInto;
+/// use kube_runtime::events::EventSource;
+///
+/// let event_source = EventSource {
+///     controller_pod: "my-awesome-controller-abcdef".try_into().unwrap(),
+///     controller: "my-awesome-controller".into(),
+/// };
+/// ```
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct EventSource {
+    /// The name of the controller publishing the event.
+    ///
+    /// E.g. `my-awesome-controller`.
+    ///
+    /// # Naming note
+    ///
+    /// `controller_name` is mapped to `reportingController` in
+    /// [`Events API`](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.22/#event-v1-events-k8s-io).
+    pub controller: String,
+    /// The name of the controller pod publishing the event.
+    ///
+    /// E.g. `my-awesome-controller-abcdef`.
+    ///
+    /// The name of the controller pod can be retrieved using Kubernetes' API or
+    /// it can be injected as an environment variable using
+    ///
+    /// ```yaml
+    /// env:
+    ///   - name: CONTROLLER_POD_NAME
+    ///     valueFrom:
+    ///       fieldRef:
+    ///         fieldPath: metadata.name
+    /// ```
+    ///
+    /// in the manifest of your controller.
+    ///
+    /// # Naming note
+    ///
+    /// `controller_pod_name` is mapped to `reportingInstance` in
+    /// [`Events API`](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.22/#event-v1-events-k8s-io).
+    pub controller_pod: ControllerPodName,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+/// The name of the controller pod publishing the event.
+///
+/// ```rust
+/// use std::convert::TryInto;
+/// use kube_runtime::events::ControllerPodName;
+///
+/// let controller_pod_name: ControllerPodName = "my-awesome-controller-abcdef".try_into().unwrap();
+/// ```
+///
+/// It must be:
+///
+/// - shorter than 128 characters.
+pub struct ControllerPodName(String);
+
+impl TryFrom<&str> for ControllerPodName {
+    type Error = ControllerPodNameParsingError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Self::try_from(value.to_string())
+    }
+}
+
+impl TryFrom<String> for ControllerPodName {
+    type Error = ControllerPodNameParsingError;
+
+    fn try_from(v: String) -> Result<Self, Self::Error> {
+        // Limit imposed by Kubernetes' API
+        let n_chars = v.chars().count();
+        if n_chars > 128 {
+            Err(ControllerPodNameParsingError {
+                controller_pod_name: v,
+            })
+        } else {
+            Ok(Self(v))
+        }
+    }
+}
+
+impl AsRef<str> for ControllerPodName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Into<String> for ControllerPodName {
+    fn into(self) -> String {
+        self.0
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct ControllerPodNameParsingError {
+    controller_pod_name: String,
+}
+
+impl std::fmt::Display for ControllerPodNameParsingError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The controller pod name must be shorter than 128 characters.")
+    }
+}
+
+impl std::error::Error for ControllerPodNameParsingError {}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+/// The reason for an action that led to a published event.
+///
+/// ```rust
+/// use std::convert::TryInto;
+/// use kube_runtime::events::EventReason;
+///
+/// let reason: EventReason = "Scheduling".try_into().unwrap();
+/// ```
+///
+/// It must be:
+///
+/// - shorter than 128 characters.
+pub struct EventReason(String);
+
+impl TryFrom<&str> for EventReason {
+    type Error = EventReasonParsingError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Self::try_from(value.to_string())
+    }
+}
+
+impl TryFrom<String> for EventReason {
+    type Error = EventReasonParsingError;
+
+    fn try_from(v: String) -> Result<Self, Self::Error> {
+        // Limit imposed by Kubernetes' API
+        let n_chars = v.chars().count();
+        if n_chars > 128 {
+            Err(EventReasonParsingError { reason: v })
+        } else {
+            Ok(Self(v))
+        }
+    }
+}
+
+impl AsRef<str> for EventReason {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Into<String> for EventReason {
+    fn into(self) -> String {
+        self.0
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct EventReasonParsingError {
+    reason: String,
+}
+
+impl std::fmt::Display for EventReasonParsingError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The reason for an event must be shorter than 128 characters.")
+    }
+}
+
+impl std::error::Error for EventReasonParsingError {}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+/// The action taken by the controller that led to a published event.
+///
+/// ```rust
+/// use std::convert::TryInto;
+/// use kube_runtime::events::EventAction;
+///
+/// let reason: EventAction = "Pulling".try_into().unwrap();
+/// ```
+///
+/// It must be:
+///
+/// - shorter than 128 characters.
+pub struct EventAction(String);
+
+impl TryFrom<&str> for EventAction {
+    type Error = EventActionParsingError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Self::try_from(value.to_string())
+    }
+}
+
+impl TryFrom<String> for EventAction {
+    type Error = EventActionParsingError;
+
+    fn try_from(v: String) -> Result<Self, Self::Error> {
+        // Limit imposed by Kubernetes' API
+        let n_chars = v.chars().count();
+        if n_chars > 128 {
+            Err(EventActionParsingError { action: v })
+        } else {
+            Ok(Self(v))
+        }
+    }
+}
+
+impl AsRef<str> for EventAction {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Into<String> for EventAction {
+    fn into(self) -> String {
+        self.0
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct EventActionParsingError {
+    action: String,
+}
+
+impl std::fmt::Display for EventActionParsingError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The action for an event must be shorter than 128 characters.")
+    }
+}
+
+impl std::error::Error for EventActionParsingError {}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+/// The human-readable message attached to a published event.
+///
+/// ```rust
+/// use std::convert::TryInto;
+/// use kube_runtime::events::EventNote;
+///
+/// let note: EventNote = "Pulling `nginx` Docker image from DockerHub.".try_into().unwrap();
+/// ```
+///
+/// It must be:
+///
+/// - smaller than 1 kilobyte.
+pub struct EventNote(String);
+
+impl TryFrom<&str> for EventNote {
+    type Error = EventNoteParsingError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Self::try_from(value.to_string())
+    }
+}
+
+impl TryFrom<String> for EventNote {
+    type Error = EventNoteParsingError;
+
+    fn try_from(v: String) -> Result<Self, Self::Error> {
+        // Limit imposed by Kubernetes' API
+        if v.len() > 1024 {
+            Err(Self::Error { note: v })
+        } else {
+            Ok(Self(v))
+        }
+    }
+}
+
+impl AsRef<str> for EventNote {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Into<String> for EventNote {
+    fn into(self) -> String {
+        self.0
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct EventNoteParsingError {
+    note: String,
+}
+
+impl std::fmt::Display for EventNoteParsingError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The note for an event must be smaller than 1 kilobyte.")
+    }
+}
+
+impl std::error::Error for EventNoteParsingError {}

kube-runtime/src/events/mod.rs
@@ -1,0 +1,5 @@
+pub use event::{ControllerPodName, EventAction, EventNote, EventReason, EventSource, EventType, NewEvent};
+pub use recorder::EventRecorder;
+
+mod event;
+mod recorder;

kube-runtime/src/events/recorder.rs
@@ -1,0 +1,106 @@
+use crate::events::{EventSource, EventType, NewEvent};
+use k8s_openapi::{
+    api::{core::v1::ObjectReference, events::v1::Event},
+    apimachinery::pkg::apis::meta::v1::{MicroTime, ObjectMeta},
+    chrono::Utc,
+};
+use kube::{api::PostParams, Api, Client};
+
+#[derive(Clone)]
+/// A publisher abstraction to emit Kubernetes' events.
+///
+/// All events emitted by an `EventRecorder` are attached to the [`ObjectReference`]
+/// specified when building the recorder using [`EventRecorder::new`].
+///
+/// ```rust
+/// use std::convert::TryInto;
+/// use kube_runtime::events::{EventSource, EventRecorder, NewEvent, EventType};
+/// use k8s_openapi::api::core::v1::ObjectReference;
+///
+/// # async fn wrapper() -> Result<(), Box<dyn std::error::Error>> {
+/// # let k8s_client: kube::Client = todo!();
+/// let event_source = EventSource {
+///     controller_pod: "my-awesome-controller-abcdef".try_into().unwrap(),
+///     controller: "my-awesome-controller".into(),
+/// };
+///
+/// // You can populate this using `ObjectMeta` and `ApiResource` information
+/// // from the object you are working with.
+/// let object_reference = ObjectReference {
+///     // [...]
+///     # ..Default::default()
+/// };
+///
+/// let event_recorder = EventRecorder::new(k8s_client, event_source, object_reference);
+/// event_recorder.publish(NewEvent {
+///     action: "Scheduling".try_into()?,
+///     reason: "Pulling".try_into()?,
+///     note: Some("Pulling image `nginx`".try_into()?),
+///     event_type: EventType::Normal,
+///     secondary_object: None,
+/// }).await?;
+/// # Ok(())
+/// # }
+/// ```
+///
+/// Events attached to an object will be shown in the `Events` section of the output of
+/// of `kubectl describe` for that object.
+pub struct EventRecorder {
+    event_client: Api<Event>,
+    event_source: EventSource,
+    object_reference: ObjectReference,
+}
+
+impl EventRecorder {
+    /// Build a new [`EventRecorder`] instance to emit events attached to the
+    /// specified [`ObjectReference`].
+    pub fn new(k8s_client: Client, event_source: EventSource, object_reference: ObjectReference) -> Self {
+        let event_client = match object_reference.namespace.as_ref() {
+            None => Api::all(k8s_client),
+            Some(namespace) => Api::namespaced(k8s_client, namespace),
+        };
+        Self {
+            event_client,
+            event_source,
+            object_reference,
+        }
+    }
+
+    /// Publish a new Kubernetes' event.
+    ///
+    /// # Access control
+    ///
+    /// The event object is created in the same namespace of the [`ObjectReference`]
+    /// you specified in [`EventRecorder::new`].
+    /// Make sure that your controller has `create` permissions in the required namespaces
+    /// for the `event` resource in the API group `events.k8s.io`.
+    pub async fn publish(&self, new_event: NewEvent) -> Result<(), kube::Error> {
+        self.event_client
+            .create(&PostParams::default(), &Event {
+                action: Some(new_event.action.into()),
+                reason: Some(new_event.reason.into()),
+                deprecated_count: None,
+                deprecated_first_timestamp: None,
+                deprecated_last_timestamp: None,
+                deprecated_source: None,
+                event_time: MicroTime(Utc::now()),
+                regarding: Some(self.object_reference.clone()),
+                note: new_event.note.map(Into::into),
+                metadata: ObjectMeta {
+                    namespace: Some(self.object_reference.namespace.clone().unwrap()),
+                    generate_name: Some(format!("{}-", self.event_source.controller)),
+                    ..Default::default()
+                },
+                reporting_controller: Some(self.event_source.controller.clone()),
+                reporting_instance: Some(self.event_source.controller_pod.clone().into()),
+                series: None,
+                type_: match new_event.event_type {
+                    EventType::Normal => Some("Normal".into()),
+                    EventType::Warning => Some("Warning".into()),
+                },
+                related: new_event.secondary_object,
+            })
+            .await?;
+        Ok(())
+    }
+}

kube-runtime/src/lib.rs
@@ -18,6 +18,7 @@
 #![allow(clippy::semicolon_if_nothing_returned)]
 
 pub mod controller;
+pub mod events;
 pub mod finalizer;
 pub mod reflector;
 pub mod scheduler;

commit a50b755e79338a7e4fda97e4e44f40455a2f41c5

Resolve Clippy errors introduced by #653
Signed-off-by: Teo Klestrup Röijezon <teo@nullable.se>

kube-runtime/src/events/event.rs
@@ -141,9 +141,9 @@ impl AsRef<str> for ControllerPodName {
     }
 }
 
-impl Into<String> for ControllerPodName {
-    fn into(self) -> String {
-        self.0
+impl From<ControllerPodName> for String {
+    fn from(val: ControllerPodName) -> Self {
+        val.0
     }
 }
 
@@ -203,9 +203,9 @@ impl AsRef<str> for EventReason {
     }
 }
 
-impl Into<String> for EventReason {
-    fn into(self) -> String {
-        self.0
+impl From<EventReason> for String {
+    fn from(val: EventReason) -> Self {
+        val.0
     }
 }
 
@@ -265,9 +265,9 @@ impl AsRef<str> for EventAction {
     }
 }
 
-impl Into<String> for EventAction {
-    fn into(self) -> String {
-        self.0
+impl From<EventAction> for String {
+    fn from(val: EventAction) -> Self {
+        val.0
     }
 }
 
@@ -326,9 +326,9 @@ impl AsRef<str> for EventNote {
     }
 }
 
-impl Into<String> for EventNote {
-    fn into(self) -> String {
-        self.0
+impl From<EventNote> for String {
+    fn from(val: EventNote) -> Self {
+        val.0
     }
 }
 

kube-runtime/src/events/mod.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::module_name_repetitions)]
+
 pub use event::{ControllerPodName, EventAction, EventNote, EventReason, EventSource, EventType, NewEvent};
 pub use recorder::EventRecorder;
 

kube-runtime/src/events/recorder.rs
@@ -54,6 +54,7 @@ pub struct EventRecorder {
 impl EventRecorder {
     /// Build a new [`EventRecorder`] instance to emit events attached to the
     /// specified [`ObjectReference`].
+    #[must_use]
     pub fn new(k8s_client: Client, event_source: EventSource, object_reference: ObjectReference) -> Self {
         let event_client = match object_reference.namespace.as_ref() {
             None => Api::all(k8s_client),
@@ -74,6 +75,10 @@ impl EventRecorder {
     /// you specified in [`EventRecorder::new`].
     /// Make sure that your controller has `create` permissions in the required namespaces
     /// for the `event` resource in the API group `events.k8s.io`.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`Error`](`kube::Error`) if the event is rejected by Kubernetes.
     pub async fn publish(&self, new_event: NewEvent) -> Result<(), kube::Error> {
         self.event_client
             .create(&PostParams::default(), &Event {
@@ -87,7 +92,7 @@ impl EventRecorder {
                 regarding: Some(self.object_reference.clone()),
                 note: new_event.note.map(Into::into),
                 metadata: ObjectMeta {
-                    namespace: Some(self.object_reference.namespace.clone().unwrap()),
+                    namespace: self.object_reference.namespace.clone(),
                     generate_name: Some(format!("{}-", self.event_source.controller)),
                     ..Default::default()
                 },