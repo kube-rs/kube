3f902623f52b615bedbaf63f37c3f6ad50c21536
Eirik A <sszynrae@gmail.com>
2021-10-20 22:35:23 +0100

    runtime::events: remove client validation + simplify module (#662)
    - simpler names for snappier docs
    - removes client side validation - see #654
    - everything in one file in root (200 lines after all)
    - avoids clippy lint
    
    Signed-off-by: clux <sszynrae@gmail.com>

--- kube-runtime/src/events/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Publishes events for objects
-#![allow(clippy::module_name_repetitions)]
-
-pub use event::{ControllerPodName, EventAction, EventNote, EventReason, EventSource, EventType, NewEvent};
-pub use recorder::EventRecorder;
-
-mod event;
-mod recorder;
