698909660f33c6881e9a7643d39b7fb978c1579d
Teo Klestrup Röijezon <teo@nullable.se>
2021-11-19 12:05:59 +0100

    Backoff tests
    Signed-off-by: Teo Klestrup Röijezon <teo@nullable.se>

--- kube-runtime/src/utils/stream_backoff.rs
+++ kube-runtime/src/utils/stream_backoff.rs
@@ -69,23 +69,108 @@ impl<S: TryStream, B: Backoff> Stream for StreamBackoff<S, B> {
         }
 
         let next_item = this.stream.try_poll_next(cx);
-        if let Poll::Ready(Some(Err(_))) = &next_item {
-            if let Some(backoff_duration) = this.backoff.next_backoff() {
-                let backoff_sleep = sleep(backoff_duration);
-                tracing::debug!(
-                    deadline = ?backoff_sleep.deadline(),
-                    duration = ?backoff_duration,
-                    "Error received, backing off"
-                );
-                this.state.set(State::BackingOff(backoff_sleep));
-            } else {
-                tracing::debug!("Error received, giving up");
-                this.state.set(State::GivenUp);
+        match &next_item {
+            Poll::Ready(Some(Err(_))) => {
+                if let Some(backoff_duration) = this.backoff.next_backoff() {
+                    let backoff_sleep = sleep(backoff_duration);
+                    tracing::debug!(
+                        deadline = ?backoff_sleep.deadline(),
+                        duration = ?backoff_duration,
+                        "Error received, backing off"
+                    );
+                    this.state.set(State::BackingOff(backoff_sleep));
+                } else {
+                    tracing::debug!("Error received, giving up");
+                    this.state.set(State::GivenUp);
+                }
             }
-        } else {
-            tracing::trace!("Non-error received, resetting backoff");
-            this.backoff.reset();
+            Poll::Ready(_) => {
+                tracing::trace!("Non-error received, resetting backoff");
+                this.backoff.reset();
+            }
+            Poll::Pending => {}
         }
         next_item
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{task::Poll, time::Duration};
+
+    use super::StreamBackoff;
+    use backoff::backoff::Backoff;
+    use futures::{channel::mpsc, pin_mut, poll, stream, StreamExt};
+
+    #[tokio::test]
+    async fn stream_should_back_off() {
+        tokio::time::pause();
+        let tick = Duration::from_secs(1);
+        let rx = stream::iter([Ok(0), Ok(1), Err(2), Ok(3), Ok(4)]);
+        let rx = StreamBackoff::new(rx, backoff::backoff::Constant::new(tick));
+        pin_mut!(rx);
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(0))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(1))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Err(2))));
+        assert_eq!(poll!(rx.next()), Poll::Pending);
+        tokio::time::advance(tick * 2).await;
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(3))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(4))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(None));
+    }
+
+    #[tokio::test]
+    async fn backoff_time_should_update() {
+        tokio::time::pause();
+        let (tx, rx) = mpsc::unbounded();
+        // let rx = stream::iter([Ok(0), Ok(1), Err(2), Ok(3)]);
+        let rx = StreamBackoff::new(rx, LinearBackoff::new(Duration::from_secs(2)));
+        pin_mut!(rx);
+        tx.unbounded_send(Ok(0)).unwrap();
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(0))));
+        tx.unbounded_send(Ok(1)).unwrap();
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(1))));
+        tx.unbounded_send(Err(2)).unwrap();
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Err(2))));
+        assert_eq!(poll!(rx.next()), Poll::Pending);
+        tokio::time::advance(Duration::from_secs(3)).await;
+        assert_eq!(poll!(rx.next()), Poll::Pending);
+        tx.unbounded_send(Err(3)).unwrap();
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Err(3))));
+        tx.unbounded_send(Ok(4)).unwrap();
+        assert_eq!(poll!(rx.next()), Poll::Pending);
+        tokio::time::advance(Duration::from_secs(3)).await;
+        assert_eq!(poll!(rx.next()), Poll::Pending);
+        tokio::time::advance(Duration::from_secs(2)).await;
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(4))));
+        assert_eq!(poll!(rx.next()), Poll::Pending);
+        drop(tx);
+        assert_eq!(poll!(rx.next()), Poll::Ready(None));
+    }
+
+    /// Dynamic backoff policy that is still deterministic and testable
+    struct LinearBackoff {
+        interval: Duration,
+        current_duration: Duration,
+    }
+
+    impl LinearBackoff {
+        fn new(interval: Duration) -> Self {
+            Self {
+                interval,
+                current_duration: Duration::ZERO,
+            }
+        }
+    }
+
+    impl Backoff for LinearBackoff {
+        fn next_backoff(&mut self) -> Option<Duration> {
+            self.current_duration += self.interval;
+            Some(self.current_duration)
+        }
+
+        fn reset(&mut self) {
+            self.current_duration = Duration::ZERO
+        }
     }
 }
