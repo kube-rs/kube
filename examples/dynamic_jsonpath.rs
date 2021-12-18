3d6ce23186630187730cc6bb5b14a34e3b9c7b63
ChinYing-Li <chinying.li@mail.utoronto.ca>
2021-12-18 08:22:00 -0500

    Modify the dynamic_jsonpath example to have a more advanced jsonpath
    Signed-off-by: ChinYing-Li <chinying.li@mail.utoronto.ca>

--- examples/dynamic_jsonpath.rs
+++ examples/dynamic_jsonpath.rs
@@ -1,11 +1,9 @@
+use jsonpath_lib;
+use k8s_openapi::api::core::v1::Pod;
 use kube::{
-    api::{Api, DynamicObject, GroupVersionKind, ListParams, ResourceExt},
-    core::{ApiResource},
-    discovery,
+    api::{Api, ListParams},
     Client,
 };
-use jsonpath_lib;
-use std::env;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -13,20 +11,22 @@ async fn main() -> anyhow::Result<()> {
     env_logger::init();
     let client = Client::try_default().await?;
 
-    // Here we simply steal the type info from k8s_openapi, but we could create this from scratch.
-    let ar = ApiResource::erase::<k8s_openapi::api::core::v1::Pod>(&());
-
+    // Equivalent to `kubectl get pods --all-namespace \
+    // -o jsonpath='{.items[*].spec.containers[*].image}'`
     let field_selector = std::env::var("FIELD_SELECTOR").unwrap_or_default();
-    let jsonpath = format!("{}{}", "$", std::env::var("JSONPATH").unwrap_or_else(|_|"@".into()));
+    let jsonpath = format!(
+        "{}{}",
+        "$",
+        std::env::var("JSONPATH").unwrap_or_else(|_| ".items[*].spec.containers[*].image".into())
+    );
 
-    // Use the discovered kind in an Api with the ApiResource as its DynamicType
-    let api = Api::<DynamicObject>::all_with(client, &ar);
+    let pods: Api<Pod> = Api::<Pod>::all(client);
     let list_params = ListParams::default().fields(&*field_selector);
-    let list = api.list(&list_params).await?;
+    let list = pods.list(&list_params).await?;
 
     // Use the given JSONPATH to filter the ObjectList
-    let items = serde_json::to_value(&list.items)?;
-    let res = jsonpath_lib::select(&items, &*jsonpath).unwrap();
+    let list_json = serde_json::to_value(&list)?;
+    let res = jsonpath_lib::select(&list_json, &*jsonpath).unwrap();
     println!("\t\t {:?}", res);
     Ok(())
 }
