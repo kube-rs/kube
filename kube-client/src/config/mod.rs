67474cdf7d20ccd4949206c86628bd208ec9ec47
Teo Klestrup Röijezon <teo@nullable.se>
2022-02-11 13:42:38 +0100

    Prefer kubeconfig over in-cluster config
    Fixes #818
    
    Signed-off-by: Teo Klestrup Röijezon <teo@nullable.se>

--- kube-client/src/config/mod.rs
+++ kube-client/src/config/mod.rs
@@ -161,23 +161,24 @@ impl Config {
 
     /// Infer the configuration from the environment
     ///
-    /// Done by attempting to load in-cluster environment variables first, and
-    /// then if that fails, trying the local kubeconfig.
+    /// Done by attempting to load the local kubec-config first, and
+    /// then if that fails, trying the in-cluster environment variables .
     ///
     /// Fails if inference from both sources fails
     ///
     /// Applies debug overrides, see [`Config::apply_debug_overrides`] for more details
     pub async fn infer() -> Result<Self, InferConfigError> {
-        let mut config = match Self::from_cluster_env() {
-            Err(in_cluster_err) => {
-                tracing::trace!("No in-cluster config found: {}", in_cluster_err);
-                tracing::trace!("Falling back to local kubeconfig");
-                Self::from_kubeconfig(&KubeConfigOptions::default())
-                    .await
-                    .map_err(|kubeconfig_err| InferConfigError {
-                        in_cluster: in_cluster_err,
-                        kubeconfig: kubeconfig_err,
-                    })?
+        let mut config = match Self::from_kubeconfig(&KubeConfigOptions::default()).await {
+            Err(kubeconfig_err) => {
+                tracing::trace!(
+                    error = &kubeconfig_err as &dyn std::error::Error,
+                    "no local config found, falling back to local in-cluster config"
+                );
+
+                Self::from_cluster_env().map_err(|in_cluster_err| InferConfigError {
+                    in_cluster: in_cluster_err,
+                    kubeconfig: kubeconfig_err,
+                })?
             }
             Ok(success) => success,
         };
