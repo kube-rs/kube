4423eaf6c9f52ff08e9c5c56dcb6775e17502ea1
Teo Klestrup Röijezon <teo@nullable.se>
2022-03-01 17:08:28 +0100

    Forcibly terminate renewer if lease cannot be renewed before it has expired

--- kube-runtime/src/lease.rs
+++ kube-runtime/src/lease.rs
@@ -9,7 +9,7 @@ use futures::{
 use k8s_openapi::{
     api::coordination::v1::{Lease, LeaseSpec},
     apimachinery::pkg::apis::meta::v1::MicroTime,
-    chrono::{DateTime, Duration, Utc},
+    chrono::{DateTime, Duration, Utc, MIN_DATETIME},
 };
 use kube_client::Api;
 
@@ -51,31 +51,51 @@ impl Elector {
     async fn keep_renewed(&self) -> RenewError {
         let watcher = watch_object(self.api.clone(), &self.name);
         let active_renewal = future::Either::Left(future::pending::<Result<Infallible, TryAcquireError>>());
-        pin_mut!(watcher, active_renewal);
+        let expiration_watchdog = future::Either::Left(future::pending::<RenewError>());
+        pin_mut!(watcher, active_renewal, expiration_watchdog);
         loop {
-            match future::select(watcher.next(), active_renewal.as_mut()).await {
+            match future::select(
+                watcher.next(),
+                future::select(active_renewal.as_mut(), expiration_watchdog.as_mut()),
+            )
+            .await
+            {
+                // Lease watcher
                 Either::Left((None, _)) => panic!("watcher should never terminate"),
                 Either::Left((Some(Err(err)), _)) => return RenewError::Watch(err),
-                Either::Left((Some(Ok(lease)), _)) => active_renewal.set(future::Either::Right(async move {
+                Either::Left((Some(Ok(lease)), _)) => {
                     let now = Utc::now();
                     let lease_state = self.state(&lease.and_then(|l| l.spec).unwrap_or_default());
-                    if let LeaseState::HeldBySelf {
-                        renew_at: Some(renew_at),
-                    } = lease_state
-                    {
-                        tracing::info!(%renew_at, "scheduling next renewal...");
-                        if let Ok(duration) = (renew_at - now).to_std() {
-                            tokio::time::sleep(duration).await;
-                        }
+                    if let LeaseState::HeldBySelf { renew_at, expires_at } = lease_state {
+                        expiration_watchdog.set(future::Either::Right(async move {
+                            tracing::debug!(%expires_at, "scheduling renewal expiration watchdog...");
+                            if let Ok(duration) = (expires_at - now).to_std() {
+                                tokio::time::sleep(duration).await;
+                            }
+                            RenewError::Timeout
+                        }));
+                        active_renewal.set(future::Either::Right(async move {
+                            tracing::info!(%renew_at, "scheduling next renewal...");
+                            if let Ok(duration) = (renew_at - now).to_std() {
+                                tokio::time::sleep(duration).await;
+                            }
+                            tracing::debug!("renewing");
+                            self.try_acquire(now).await?;
+                            tracing::debug!("renew finished");
+                            // watcher should emit the new lease, scheduling a new renewal
+                            future::pending().await
+                        }))
+                    } else {
+                        return RenewError::Lost;
                     }
-                    tracing::debug!("renewing");
-                    self.try_acquire(now).await?;
-                    tracing::debug!("renew finished");
-                    // watcher should emit the new lease, scheduling a new renewal
-                    future::pending().await
-                })),
-                Either::Right((Err(err), _)) => return RenewError::Acquire(err),
-                Either::Right((Ok(x), _)) => match x {},
+                }
+
+                // Renewer
+                Either::Right((Either::Left((Err(err), _)), _)) => return RenewError::Acquire(err),
+                Either::Right((Either::Left((Ok(x), _)), _)) => match x {},
+
+                // Watchdog
+                Either::Right((Either::Right((err, _)), _)) => return err,
             }
         }
     }
@@ -98,13 +118,9 @@ impl Elector {
 
                     active_acquisition.set(future::Either::Right(async move {
                         let now = Utc::now();
-                        if let LeaseState::HeldByOther {
-                            holder,
-                            expires_at: Some(renew_at),
-                        } = lease_state
-                        {
-                            tracing::info!(?holder, %renew_at, "scheduling next acquisition attempt...");
-                            if let Ok(duration) = (renew_at - now).to_std() {
+                        if let LeaseState::HeldByOther { holder, expires_at } = lease_state {
+                            tracing::info!(?holder, %expires_at, "scheduling next acquisition attempt...");
+                            if let Ok(duration) = (expires_at - now).to_std() {
                                 tokio::time::sleep(duration).await;
                             }
                         }
@@ -137,7 +153,7 @@ impl Elector {
 
         if let LeaseState::HeldByOther {
             ref holder,
-            expires_at: Some(expires_at),
+            expires_at,
         } = lease_state
         {
             if expires_at > now {
@@ -189,32 +205,18 @@ impl Elector {
     }
 
     fn state(&self, lease: &LeaseSpec) -> LeaseState {
+        let lease_duration = Duration::seconds(lease.lease_duration_seconds.unwrap_or(0).into());
+        let last_renewal = lease.renew_time.as_ref().map_or(MIN_DATETIME, |dt| dt.0);
+
         match &lease.holder_identity {
             None => LeaseState::Unheld,
             Some(holder) if holder == &self.identity => LeaseState::HeldBySelf {
-                renew_at: if let LeaseSpec {
-                    lease_duration_seconds: Some(duration_secs),
-                    renew_time: Some(renew_time),
-                    ..
-                } = lease
-                {
-                    Some(renew_time.0 + Duration::seconds((*duration_secs).into()) / 2)
-                } else {
-                    None
-                },
+                expires_at: last_renewal + lease_duration,
+                renew_at: last_renewal + lease_duration / 2,
             },
             Some(holder) => LeaseState::HeldByOther {
                 holder: holder.clone(),
-                expires_at: if let LeaseSpec {
-                    lease_duration_seconds: Some(duration_secs),
-                    renew_time: Some(renew_time),
-                    ..
-                } = lease
-                {
-                    Some(renew_time.0 + Duration::seconds((*duration_secs).into()))
-                } else {
-                    None
-                },
+                expires_at: last_renewal + lease_duration,
             },
         }
     }
@@ -225,10 +227,11 @@ enum LeaseState {
     Unheld,
     HeldByOther {
         holder: String,
-        expires_at: Option<DateTime<Utc>>,
+        expires_at: DateTime<Utc>,
     },
     HeldBySelf {
-        renew_at: Option<DateTime<Utc>>,
+        renew_at: DateTime<Utc>,
+        expires_at: DateTime<Utc>,
     },
 }
 
@@ -259,6 +262,8 @@ pub enum ReleaseError {
 pub enum RenewError {
     Watch(watcher::Error),
     Acquire(TryAcquireError),
+    Timeout,
+    Lost,
 }
 
 #[derive(Debug, thiserror::Error)]
