4feb7ddda822d9fb0e645a0aa0fd3536086c4e52
clux <sszynrae@gmail.com>
2022-06-14 09:03:50 +0100

    remove duplicate changes from #924
    (won't build until that is merged)
    
    Signed-off-by: clux <sszynrae@gmail.com>

--- examples/job_api.rs
+++ examples/job_api.rs
@@ -1,24 +1,25 @@
+use futures::{StreamExt, TryStreamExt};
 use k8s_openapi::api::batch::v1::Job;
+use serde_json::json;
+use tracing::*;
+
 use kube::{
-    api::{Api, DeleteParams, PostParams},
-    runtime::wait::{await_condition, conditions},
+    api::{Api, DeleteParams, ListParams, PostParams, ResourceExt, WatchEvent},
     Client,
 };
-use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let client = Client::try_default().await?;
-    let jobs: Api<Job> = Api::default_namespaced(client);
 
-    info!("Creating job");
-    let name = "empty-job";
-    let data = serde_json::from_value(serde_json::json!({
+    // Create a Job
+    let job_name = "empty-job";
+    let my_job = serde_json::from_value(json!({
         "apiVersion": "batch/v1",
         "kind": "Job",
         "metadata": {
-            "name": name,
+            "name": job_name,
         },
         "spec": {
             "template": {
@@ -35,13 +36,41 @@ async fn main() -> anyhow::Result<()> {
             }
         }
     }))?;
-    jobs.create(&PostParams::default(), &data).await?;
 
-    info!("Waiting for job to complete");
-    let cond = await_condition(jobs.clone(), name, conditions::is_job_completed());
-    let _ = tokio::time::timeout(std::time::Duration::from_secs(20), cond).await?;
+    let jobs: Api<Job> = Api::default_namespaced(client);
+    let pp = PostParams::default();
 
-    info!("Cleaning up job record");
-    jobs.delete(name, &DeleteParams::background()).await?;
+    jobs.create(&pp, &my_job).await?;
+
+    // See if it ran to completion
+    let lp = ListParams::default()
+        .fields(&format!("metadata.name={}", job_name)) // only want events for our job
+        .timeout(20); // should be done by then
+    let mut stream = jobs.watch(&lp, "").await?.boxed();
+
+    while let Some(status) = stream.try_next().await? {
+        match status {
+            WatchEvent::Added(s) => info!("Added {}", s.name()),
+            WatchEvent::Modified(s) => {
+                let current_status = s.status.clone().expect("Status is missing");
+                match current_status.completion_time {
+                    Some(_) => {
+                        info!("Modified: {} is complete", s.name());
+                        break;
+                    }
+                    _ => info!("Modified: {} is running", s.name()),
+                }
+            }
+            WatchEvent::Deleted(s) => info!("Deleted {}", s.name()),
+            WatchEvent::Error(s) => error!("{}", s),
+            _ => {}
+        }
+    }
+
+    // Clean up the old job record..
+    info!("Deleting the job record.");
+    jobs.delete("empty-job", &DeleteParams::background().dry_run())
+        .await?;
+    jobs.delete("empty-job", &DeleteParams::background()).await?;
     Ok(())
 }
