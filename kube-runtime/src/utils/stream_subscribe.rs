8d77e0a3ab747d68f38cbb1fdf49d9d4205382ce
Dan Spencer <danrspen@gmail.com>
2023-02-13 11:12:16 +0000

    StreamSubscribe now wraps items in arcs so that:
    1. Remove expensive cloning of Kubernetes objects
    2. Allow propogation of err events
    
    Signed-off-by: Dan Spencer <danrspen@gmail.com>

--- kube-runtime/src/utils/stream_subscribe.rs
+++ kube-runtime/src/utils/stream_subscribe.rs
@@ -4,6 +4,7 @@ use core::{
 };
 use futures::{stream, Stream, TryStream};
 use pin_project::pin_project;
+use std::sync::Arc;
 use tokio::sync::broadcast;
 
 /// Exposes the [`StreamSubscribe::subscribe_ok()`] method that allows additional
@@ -19,21 +20,18 @@ where
 {
     #[pin]
     stream: S,
-    sender: broadcast::Sender<Option<S::Ok>>,
+    sender: broadcast::Sender<Option<Arc<Result<S::Ok, S::Error>>>>,
 }
 
-impl<S: TryStream> StreamSubscribe<S>
-where
-    S::Ok: Clone,
-{
+impl<S: TryStream> StreamSubscribe<S> {
     pub fn new(stream: S) -> Self {
         let (sender, _) = broadcast::channel(100);
 
         Self { stream, sender }
     }
 
-    /// Subscribe to success events from this stream
-    pub fn subscribe_ok(&self) -> impl Stream<Item = S::Ok> {
+    /// Subscribe to events from this stream
+    pub fn subscribe(&self) -> impl Stream<Item = Arc<Result<S::Ok, S::Error>>> {
         stream::unfold(self.sender.subscribe(), |mut rx| async move {
             match rx.recv().await {
                 Ok(Some(obj)) => Some((obj, rx)),
@@ -43,25 +41,25 @@ where
     }
 }
 
-impl<S: TryStream> Stream for StreamSubscribe<S>
-where
-    S::Ok: Clone,
-{
-    type Item = Result<S::Ok, S::Error>;
+impl<S: TryStream> Stream for StreamSubscribe<S> {
+    type Item = Arc<Result<S::Ok, S::Error>>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
         let item = this.stream.try_poll_next(cx);
 
-        if let Poll::Ready(Some(Ok(item))) = &item {
-            this.sender.send(Some((*item).clone())).ok();
-        } else if let Poll::Ready(None) = &item {
-            // If the stream is closed, we need to send a None to all subscribers
-            // which will cause them to close.
-            this.sender.send(None).ok();
+        match item {
+            Poll::Ready(Some(item)) => {
+                let item = Arc::new(item);
+                this.sender.send(Some(item.clone())).ok();
+                Poll::Ready(Some(item))
+            }
+            Poll::Ready(None) => {
+                this.sender.send(None).ok();
+                Poll::Ready(None)
+            }
+            Poll::Pending => Poll::Pending,
         }
-
-        item
     }
 }
 
@@ -76,41 +74,42 @@ mod tests {
         let rx = StreamSubscribe::new(rx);
 
         pin_mut!(rx);
-        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(0))));
-        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(1))));
-        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Err(2))));
-        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(3))));
-        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(4))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Arc::new(Ok(0)))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Arc::new(Ok(1)))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Arc::new(Err(2)))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Arc::new(Ok(3)))));
+        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Arc::new(Ok(4)))));
         assert_eq!(poll!(rx.next()), Poll::Ready(None));
     }
 
     #[tokio::test]
-    async fn all_subscribers_get_success_events() {
-        let rx = stream::iter([Ok(0), Err(1)]);
+    async fn all_subscribers_get_events() {
+        let items = [Ok(0), Ok(1), Err(2), Ok(3), Ok(4)];
+        let rx = stream::iter(items.clone());
         let rx = StreamSubscribe::new(rx);
 
-        let rx_s1 = rx.subscribe_ok();
-        let rx_s2 = rx.subscribe_ok();
+        let rx_s1 = rx.subscribe();
+        let rx_s2 = rx.subscribe();
 
         pin_mut!(rx);
         pin_mut!(rx_s1);
         pin_mut!(rx_s2);
 
-        assert_eq!(poll!(rx_s1.next()), Poll::Pending, "rx_s1");
-        assert_eq!(poll!(rx_s2.next()), Poll::Pending, "rx_s2");
+        // Subscribers are pending until we start consuming the stream
+        assert_eq!(poll!(rx_s1.next()), Poll::Pending, "rx_s1 - pending");
+        assert_eq!(poll!(rx_s2.next()), Poll::Pending, "rx_s2 - pending");
 
-        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Ok(0))), "rx");
-        assert_eq!(poll!(rx_s1.next()), Poll::Ready(Some(0)), "rx_s1");
-        assert_eq!(poll!(rx_s2.next()), Poll::Ready(Some(0)), "rx_s2");
-
-        // Subscribers don't get error events
-        assert_eq!(poll!(rx.next()), Poll::Ready(Some(Err(1))), "rx");
-        assert_eq!(poll!(rx_s1.next()), Poll::Pending, "rx_s1");
-        assert_eq!(poll!(rx_s1.next()), Poll::Pending, "rx_s2");
+        for (index, item) in items.into_iter().enumerate() {
+            let expected = Poll::Ready(Some(Arc::new(item)));
+            assert_eq!(poll!(rx.next()), expected, "rx - {}", index);
+            assert_eq!(poll!(rx_s1.next()), expected, "rx_s1 - {}", index);
+            assert_eq!(poll!(rx_s2.next()), expected, "rx_s2 - {}", index);
+        }
 
         // Ensure that if the stream is closed, all subscribers are closed
-        assert_eq!(poll!(rx.next()), Poll::Ready(None), "rx");
-        assert_eq!(poll!(rx_s1.next()), Poll::Ready(None), "rx_s1");
-        assert_eq!(poll!(rx_s2.next()), Poll::Ready(None), "rx_s2");
+        let expected = Poll::Ready(None);
+        assert_eq!(poll!(rx.next()), expected, "rx - close");
+        assert_eq!(poll!(rx_s1.next()), expected, "rx_s1 - close");
+        assert_eq!(poll!(rx_s2.next()), expected, "rx_s2 - close");
     }
 }
