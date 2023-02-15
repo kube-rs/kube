a85fe3fbecbd9e0f4a44c61402941a562a663065
Dan Spencer <danrspen@gmail.com>
2023-02-15 13:21:57 +0000

    StreamSubscribe now allows subscribers how to handle lagging
    Signed-off-by: Dan Spencer <danrspen@gmail.com>

--- kube-runtime/src/utils/stream_subscribe.rs
+++ kube-runtime/src/utils/stream_subscribe.rs
@@ -2,51 +2,60 @@ use core::{
     pin::Pin,
     task::{Context, Poll},
 };
-use futures::{stream, Stream, TryStream};
+use futures::{stream, Stream};
 use pin_project::pin_project;
+use std::fmt;
 use std::sync::Arc;
 use tokio::sync::broadcast;
+use tokio::sync::broadcast::error::RecvError;
 
-/// Exposes the [`StreamSubscribe::subscribe_ok()`] method that allows additional
-/// consumers of [`Ok`] events from a stream without consuming the stream itself.
+const CHANNEL_CAPACITY: usize = 128;
+
+/// Exposes the [`StreamSubscribe::subscribe()`] method which allows additional
+/// consumers of events from a stream without consuming the stream itself.
 ///
-/// If the [`Stream`] is dropped or ends, any [`StreamSubscribe::subscribe_ok()`] streams
+/// If a subscriber beings to lag behind the stream, it will receive a [`StreamSubscribeError::Lagged`]
+/// error. The subscriber can then decide to abort its or tolerate the lost events.
+///
+/// If the [`Stream`] is dropped or ends, any [`StreamSubscribe::subscribe()`] streams
 /// will also end.
 #[pin_project]
-#[must_use = "streams do nothing unless polled"]
+#[must_use = "subscribers will not get events unless this stream is polled"]
 pub struct StreamSubscribe<S>
 where
-    S: TryStream,
+    S: Stream,
 {
     #[pin]
     stream: S,
-    sender: broadcast::Sender<Option<Arc<Result<S::Ok, S::Error>>>>,
+    sender: broadcast::Sender<Option<Arc<S::Item>>>,
 }
 
-impl<S: TryStream> StreamSubscribe<S> {
+impl<S: Stream> StreamSubscribe<S> {
     pub fn new(stream: S) -> Self {
-        let (sender, _) = broadcast::channel(100);
+        let (sender, _) = broadcast::channel(CHANNEL_CAPACITY);
 
         Self { stream, sender }
     }
 
     /// Subscribe to events from this stream
-    pub fn subscribe(&self) -> impl Stream<Item = Arc<Result<S::Ok, S::Error>>> {
+    #[must_use = "streams do nothing unless polled"]
+    pub fn subscribe(&self) -> impl Stream<Item = Result<Arc<S::Item>, StreamSubscribeError>> {
         stream::unfold(self.sender.subscribe(), |mut rx| async move {
             match rx.recv().await {
-                Ok(Some(obj)) => Some((obj, rx)),
+                Ok(Some(obj)) => Some((Ok(obj), rx)),
+                Err(RecvError::Lagged(amt)) => Some((Err(StreamSubscribeError::Lagged(amt)), rx)),
                 _ => None,
             }
         })
     }
 }
 
-impl<S: TryStream> Stream for StreamSubscribe<S> {
-    type Item = Arc<Result<S::Ok, S::Error>>;
+impl<S: Stream> Stream for StreamSubscribe<S> {
+    type Item = Arc<S::Item>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
-        let item = this.stream.try_poll_next(cx);
+        let item = this.stream.poll_next(cx);
 
         match item {
             Poll::Ready(Some(item)) => {
@@ -62,6 +71,26 @@ impl<S: TryStream> Stream for StreamSubscribe<S> {
         }
     }
 }
+
+/// An error returned from the inner stream of a [`StreamSubscribe`].
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum StreamSubscribeError {
+    /// The subscriber lagged too far behind. Polling again will return
+    /// the oldest event still retained.
+    ///
+    /// Includes the number of skipped events.
+    Lagged(u64),
+}
+
+impl fmt::Display for StreamSubscribeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            StreamSubscribeError::Lagged(amt) => write!(f, "subscriber lagged by {}", amt),
+        }
+    }
+}
+
+impl std::error::Error for StreamSubscribeError {}
 
 #[cfg(test)]
 mod tests {
@@ -84,8 +113,8 @@ mod tests {
 
     #[tokio::test]
     async fn all_subscribers_get_events() {
-        let items = [Ok(0), Ok(1), Err(2), Ok(3), Ok(4)];
-        let rx = stream::iter(items.clone());
+        let events = [Ok(0), Ok(1), Err(2), Ok(3), Ok(4)];
+        let rx = stream::iter(events.clone());
         let rx = StreamSubscribe::new(rx);
 
         let rx_s1 = rx.subscribe();
@@ -96,20 +125,113 @@ mod tests {
         pin_mut!(rx_s2);
 
         // Subscribers are pending until we start consuming the stream
-        assert_eq!(poll!(rx_s1.next()), Poll::Pending, "rx_s1 - pending");
-        assert_eq!(poll!(rx_s2.next()), Poll::Pending, "rx_s2 - pending");
+        assert_eq!(poll!(rx_s1.next()), Poll::Pending, "rx_s1");
+        assert_eq!(poll!(rx_s2.next()), Poll::Pending, "rx_s2");
 
-        for (index, item) in items.into_iter().enumerate() {
-            let expected = Poll::Ready(Some(Arc::new(item)));
-            assert_eq!(poll!(rx.next()), expected, "rx - {}", index);
-            assert_eq!(poll!(rx_s1.next()), expected, "rx_s1 - {}", index);
-            assert_eq!(poll!(rx_s2.next()), expected, "rx_s2 - {}", index);
+        for item in events {
+            assert_eq!(poll!(rx.next()), Poll::Ready(Some(Arc::new(item))), "rx");
+            let expected = Poll::Ready(Some(Ok(Arc::new(item))));
+            assert_eq!(poll!(rx_s1.next()), expected, "rx_s1");
+            assert_eq!(poll!(rx_s2.next()), expected, "rx_s2");
         }
 
         // Ensure that if the stream is closed, all subscribers are closed
-        let expected = Poll::Ready(None);
-        assert_eq!(poll!(rx.next()), expected, "rx - close");
-        assert_eq!(poll!(rx_s1.next()), expected, "rx_s1 - close");
-        assert_eq!(poll!(rx_s2.next()), expected, "rx_s2 - close");
+        assert_eq!(poll!(rx.next()), Poll::Ready(None), "rx");
+        assert_eq!(poll!(rx_s1.next()), Poll::Ready(None), "rx_s1");
+        assert_eq!(poll!(rx_s2.next()), Poll::Ready(None), "rx_s2");
+    }
+
+    #[tokio::test]
+    async fn subscribers_can_catch_up_to_the_main_stream() {
+        let events = (0..CHANNEL_CAPACITY).map(Ok::<_, ()>).collect::<Vec<_>>();
+        let rx = stream::iter(events.clone());
+        let rx = StreamSubscribe::new(rx);
+
+        let rx_s1 = rx.subscribe();
+
+        pin_mut!(rx);
+        pin_mut!(rx_s1);
+
+        for item in events.clone() {
+            assert_eq!(poll!(rx.next()), Poll::Ready(Some(Arc::new(item))), "rx",);
+        }
+
+        for item in events {
+            assert_eq!(
+                poll!(rx_s1.next()),
+                Poll::Ready(Some(Ok(Arc::new(item)))),
+                "rx_s1"
+            );
+        }
+    }
+
+    #[tokio::test]
+    async fn if_the_subscribers_lag_they_get_a_lagged_error_as_the_next_event() {
+        // The broadcast channel rounds the capacity up to the next power of two.
+        let max_capacity = CHANNEL_CAPACITY.next_power_of_two();
+        let overflow = 5;
+        let events = (0..max_capacity + overflow).collect::<Vec<_>>();
+        let rx = stream::iter(events.clone());
+        let rx = StreamSubscribe::new(rx);
+
+        let rx_s1 = rx.subscribe();
+
+        pin_mut!(rx);
+        pin_mut!(rx_s1);
+
+        // Consume the entire stream, overflowing the inner channel
+        for _ in events {
+            let _ = poll!(rx.next());
+        }
+
+        assert_eq!(
+            poll!(rx_s1.next()),
+            Poll::Ready(Some(Err(StreamSubscribeError::Lagged(overflow as u64)))),
+        );
+
+        let expected_next_event = overflow;
+        assert_eq!(
+            poll!(rx_s1.next()),
+            Poll::Ready(Some(Ok(Arc::new(expected_next_event)))),
+        );
+    }
+
+    #[tokio::test]
+    async fn a_lagging_subscriber_does_not_impact_a_well_behaved_subscriber() {
+        // The broadcast channel rounds the capacity up to the next power of two.
+        let max_capacity = CHANNEL_CAPACITY.next_power_of_two();
+        let overflow = 5;
+        let events = (0..max_capacity + overflow).collect::<Vec<_>>();
+        let rx = stream::iter(events.clone());
+        let rx = StreamSubscribe::new(rx);
+
+        let rx_s1 = rx.subscribe();
+        let rx_s2 = rx.subscribe();
+
+        pin_mut!(rx);
+        pin_mut!(rx_s1);
+        pin_mut!(rx_s2);
+
+        for event in events {
+            let _ = poll!(rx.next());
+            assert_eq!(
+                poll!(rx_s1.next()),
+                Poll::Ready(Some(Ok(Arc::new(event)))),
+                "rx_s1"
+            );
+        }
+
+        assert_eq!(
+            poll!(rx_s2.next()),
+            Poll::Ready(Some(Err(StreamSubscribeError::Lagged(overflow as u64)))),
+            "rx_s2"
+        );
+
+        let expected_next_event = overflow;
+        assert_eq!(
+            poll!(rx_s2.next()),
+            Poll::Ready(Some(Ok(Arc::new(expected_next_event)))),
+            "rx_s2"
+        );
     }
 }
