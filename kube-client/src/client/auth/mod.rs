1d18d31ded50d8ca8059068ef8c83ede6422c877
Razz4780 <msmolarekg@gmail.com>
2023-06-26 11:00:32 +0200

    Feature rename and propagation to 'kube' crate
    Signed-off-by: Razz4780 <msmolarekg@gmail.com>

--- kube-client/src/client/auth/mod.rs
+++ kube-client/src/client/auth/mod.rs
@@ -22,8 +22,8 @@ use crate::config::{AuthInfo, AuthProviderConfig, ExecConfig, ExecInteractiveMod
 
 #[cfg(feature = "oauth")] mod oauth;
 #[cfg(feature = "oauth")] pub use oauth::Error as OAuthError;
-#[cfg(feature = "refresh-oidc")] mod oidc;
-#[cfg(feature = "refresh-oidc")] pub use oidc::Error as OidcError;
+#[cfg(feature = "oidc")] mod oidc;
+#[cfg(feature = "oidc")] pub use oidc::Error as OidcError;
 #[cfg(target_os = "windows")] use std::os::windows::process::CommandExt;
 
 #[derive(Error, Debug)]
@@ -95,8 +95,8 @@ pub enum Error {
     OAuth(#[source] OAuthError),
 
     /// OIDC error
-    #[cfg(feature = "refresh-oidc")]
-    #[cfg_attr(docsrs, doc(cfg(feature = "refresh-oidc")))]
+    #[cfg(feature = "oidc")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "oidc")))]
     #[error("failed OIDC: {0}")]
     Oidc(#[source] OidcError),
 }
@@ -172,7 +172,7 @@ pub enum RefreshableToken {
     File(Arc<RwLock<TokenFile>>),
     #[cfg(feature = "oauth")]
     GcpOauth(Arc<Mutex<oauth::Gcp>>),
-    #[cfg(feature = "refresh-oidc")]
+    #[cfg(feature = "oidc")]
     Oidc(Arc<Mutex<oidc::Oidc>>),
 }
 
@@ -222,7 +222,7 @@ impl RefreshableToken {
                         Auth::RefreshableToken(RefreshableToken::File(_)) => unreachable!(),
                         #[cfg(feature = "oauth")]
                         Auth::RefreshableToken(RefreshableToken::GcpOauth(_)) => unreachable!(),
-                        #[cfg(feature = "refresh-oidc")]
+                        #[cfg(feature = "oidc")]
                         Auth::RefreshableToken(RefreshableToken::Oidc(_)) => unreachable!(),
                     }
                 }
@@ -249,7 +249,7 @@ impl RefreshableToken {
                 bearer_header(&token.access_token)
             }
 
-            #[cfg(feature = "refresh-oidc")]
+            #[cfg(feature = "oidc")]
             RefreshableToken::Oidc(oidc) => {
                 let token = oidc.lock().await.id_token().await.map_err(Error::Oidc)?;
                 bearer_header(&token)
@@ -273,14 +273,14 @@ impl TryFrom<&AuthInfo> for Auth {
     fn try_from(auth_info: &AuthInfo) -> Result<Self, Self::Error> {
         if let Some(provider) = &auth_info.auth_provider {
             match token_from_provider(provider)? {
-                #[cfg(feature = "refresh-oidc")]
+                #[cfg(feature = "oidc")]
                 ProviderToken::Oidc(oidc) => {
                     return Ok(Self::RefreshableToken(RefreshableToken::Oidc(Arc::new(
                         Mutex::new(oidc),
                     ))));
                 }
 
-                #[cfg(not(feature = "refresh-oidc"))]
+                #[cfg(not(feature = "oidc"))]
                 ProviderToken::Oidc(token) => {
                     return Ok(Self::Bearer(SecretString::from(token)));
                 }
@@ -353,9 +353,9 @@ impl TryFrom<&AuthInfo> for Auth {
 
 // We need to differentiate providers because the keys/formats to store token expiration differs.
 enum ProviderToken {
-    #[cfg(feature = "refresh-oidc")]
+    #[cfg(feature = "oidc")]
     Oidc(oidc::Oidc),
-    #[cfg(not(feature = "refresh-oidc"))]
+    #[cfg(not(feature = "oidc"))]
     Oidc(String),
     // "access-token", "expiry" (RFC3339)
     GcpCommand(String, Option<DateTime<Utc>>),
@@ -379,14 +379,14 @@ fn token_from_provider(provider: &AuthProviderConfig) -> Result<ProviderToken, E
     }
 }
 
-#[cfg(feature = "refresh-oidc")]
+#[cfg(feature = "oidc")]
 fn token_from_oidc_provider(provider: &AuthProviderConfig) -> Result<ProviderToken, Error> {
     oidc::Oidc::from_config(&provider.config)
         .map_err(Error::Oidc)
         .map(ProviderToken::Oidc)
 }
 
-#[cfg(not(feature = "refresh-oidc"))]
+#[cfg(not(feature = "oidc"))]
 fn token_from_oidc_provider(provider: &AuthProviderConfig) -> Result<ProviderToken, Error> {
     match provider.config.get("id-token") {
         Some(id_token) => Ok(ProviderToken::Oidc(id_token.clone())),
