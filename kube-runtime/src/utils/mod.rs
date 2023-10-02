5ab17d63538e53264a4fb898bab6f6c1d0731d89
clux <sszynrae@gmail.com>
2023-10-02 20:21:19 +0100

    avoid some complicated clippy issues
    Signed-off-by: clux <sszynrae@gmail.com>

--- kube-runtime/src/utils/mod.rs
+++ kube-runtime/src/utils/mod.rs
@@ -105,7 +105,7 @@ where
 /// Splits a `TryStream` into separate `Ok` and `Error` streams.
 ///
 /// Note: This will deadlock if one branch outlives the other
-#[allow(clippy::type_complexity)]
+#[allow(clippy::type_complexity, clippy::arc_with_non_send_sync)]
 fn trystream_split_result<S>(
     stream: S,
 ) -> (
