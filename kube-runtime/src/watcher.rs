b256a5318aa8bff38199011df8e6cddea46b505a
clux <sszynrae@gmail.com>
2024-06-19 17:13:14 +0100

    Fix watcher not fully paginating on Init
    cannot believe i missed this :(
    fixes #1524
    
    Signed-off-by: clux <sszynrae@gmail.com>

--- kube-runtime/src/watcher.rs
+++ kube-runtime/src/watcher.rs
@@ -499,9 +499,12 @@ where
                     last_bookmark,
                 });
             }
-            if let Some(resource_version) = last_bookmark {
-                // we have drained the last page - move on to next stage
-                return (Some(Ok(Event::InitDone)), State::InitListed { resource_version });
+            // check if we need to perform more pages
+            if continue_token.is_none() {
+                if let Some(resource_version) = last_bookmark {
+                    // we have drained the last page - move on to next stage
+                    return (Some(Ok(Event::InitDone)), State::InitListed { resource_version });
+                }
             }
             let mut lp = wc.to_list_params();
             lp.continue_token = continue_token;
