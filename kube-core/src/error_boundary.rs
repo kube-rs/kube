8d4d7508aec238fa2d73549020fd51f8a7dac49e
Natalie Klestrup Röijezon <nat@nullable.se>
2024-08-07 09:07:01 +0200

    s/T/K/g
    Signed-off-by: Natalie Klestrup Röijezon <nat@nullable.se>

--- kube-core/src/error_boundary.rs
+++ kube-core/src/error_boundary.rs
@@ -8,13 +8,13 @@ use serde_value::DeserializerError;
 
 use crate::{PartialObjectMeta, Resource};
 
-/// A wrapper type for T that lets deserializing the parent object succeed, even if the T is invalid.
+/// A wrapper type for K that lets deserializing the parent object succeed, even if the K is invalid.
 ///
 /// For example, this can be used to still access valid objects from an `Api::list` call or `watcher`.
-// We can't implement Deserialize on Result<T, InvalidObject> directly, both because of the orphan rule and because
-// it would conflict with serde's blanket impl on Result<T, E>, even if E isn't Deserialize.
+// We can't implement Deserialize on Result<K, InvalidObject> directly, both because of the orphan rule and because
+// it would conflict with serde's blanket impl on Result<K, E>, even if E isn't Deserialize.
 #[derive(Debug, Clone)]
-pub struct ErrorBoundary<T>(pub Result<T, InvalidObject>);
+pub struct ErrorBoundary<K>(pub Result<K, InvalidObject>);
 
 /// An object that failed to be deserialized by the [`ErrorBoundary`].
 #[derive(Debug, Clone)]
@@ -33,11 +33,11 @@ impl Display for InvalidObject {
     }
 }
 
-impl<'de, T> Deserialize<'de> for ErrorBoundary<T>
+impl<'de, K> Deserialize<'de> for ErrorBoundary<K>
 where
-    T: Deserialize<'de>,
-    // Not actually used, but we assume that T is a Kubernetes-style resource with a `metadata` section
-    T: Resource,
+    K: Deserialize<'de>,
+    // Not actually used, but we assume that K is a Kubernetes-style resource with a `metadata` section
+    K: Resource,
 {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
@@ -48,11 +48,11 @@ where
         let buffer = serde_value::Value::deserialize(deserializer)?;
 
         // FIXME: can we avoid cloning the whole object? metadata should be enough, and even then we could prune managedFields
-        T::deserialize(buffer.clone())
+        K::deserialize(buffer.clone())
             .map(Ok)
             .or_else(|err| {
                 let PartialObjectMeta { metadata, .. } =
-                    PartialObjectMeta::<T>::deserialize(buffer).map_err(DeserializerError::into_error)?;
+                    PartialObjectMeta::<K>::deserialize(buffer).map_err(DeserializerError::into_error)?;
                 Ok(Err(InvalidObject {
                     error: err.to_string(),
                     metadata,
@@ -62,32 +62,32 @@ where
     }
 }
 
-impl<T: Resource> Resource for ErrorBoundary<T> {
-    type DynamicType = T::DynamicType;
-    type Scope = T::Scope;
+impl<K: Resource> Resource for ErrorBoundary<K> {
+    type DynamicType = K::DynamicType;
+    type Scope = K::Scope;
 
     fn kind(dt: &Self::DynamicType) -> Cow<str> {
-        T::kind(dt)
+        K::kind(dt)
     }
 
     fn group(dt: &Self::DynamicType) -> Cow<str> {
-        T::group(dt)
+        K::group(dt)
     }
 
     fn version(dt: &Self::DynamicType) -> Cow<str> {
-        T::version(dt)
+        K::version(dt)
     }
 
     fn plural(dt: &Self::DynamicType) -> Cow<str> {
-        T::plural(dt)
+        K::plural(dt)
     }
 
     fn meta(&self) -> &ObjectMeta {
-        self.0.as_ref().map_or_else(|err| &err.metadata, T::meta)
+        self.0.as_ref().map_or_else(|err| &err.metadata, K::meta)
     }
 
     fn meta_mut(&mut self) -> &mut ObjectMeta {
-        self.0.as_mut().map_or_else(|err| &mut err.metadata, T::meta_mut)
+        self.0.as_mut().map_or_else(|err| &mut err.metadata, K::meta_mut)
     }
 }
 
