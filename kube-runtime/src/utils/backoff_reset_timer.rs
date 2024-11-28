11e9045e14ccbbfc4ff1dbc301fd9e446544b64d
Natalie Klestrup Röijezon <nat@nullable.se>
2024-11-28 16:11:36 +0100

    Remove Clock
    Signed-off-by: Natalie Klestrup Röijezon <nat@nullable.se>

--- kube-runtime/src/utils/backoff_reset_timer.rs
+++ kube-runtime/src/utils/backoff_reset_timer.rs
@@ -4,72 +4,53 @@ use backon::BackoffBuilder;
 
 use super::{ResettableBackoff, ResettableBackoffWrapper};
 
-// TODO: do we actually need this or should we just use tokio::time?
-pub trait Clock: Send + Sync + Unpin {
-    fn now(&self) -> Instant;
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct TokioClock;
-impl Clock for TokioClock {
-    fn now(&self) -> Instant {
-        tokio::time::Instant::now().into_std()
-    }
+/// Builder for [`ResetTimerBackoff`].
+#[derive(Debug, Clone)]
+pub struct ResetTimerBackoffBuilder<B> {
+    inner_backoff_builder: B,
+    reset_duration: Duration,
 }
 
 impl<B: BackoffBuilder> ResetTimerBackoffBuilder<B> {
     pub fn new(inner_backoff_builder: B, reset_duration: Duration) -> Self {
-        Self::new_with_custom_clock(inner_backoff_builder, reset_duration, TokioClock)
-    }
-}
-
-impl<B: BackoffBuilder, C: Clock> ResetTimerBackoffBuilder<B, C> {
-    fn new_with_custom_clock(inner_backoff_builder: B, reset_duration: Duration, clock: C) -> Self {
         Self {
             inner_backoff_builder,
-            clock,
             reset_duration,
         }
     }
 }
 
-/// A [`Backoff`] wrapper that resets after a fixed duration has elapsed.
-#[derive(Debug, Clone)]
-pub struct ResetTimerBackoffBuilder<B, C = TokioClock> {
-    inner_backoff_builder: B,
-    clock: C,
-    reset_duration: Duration,
-}
-
-impl<B: BackoffBuilder + Clone, C: Clock> BackoffBuilder for ResetTimerBackoffBuilder<B, C> {
-    type Backoff = ResetTimerBackoff<ResettableBackoffWrapper<B>, C>;
+impl<B: BackoffBuilder + Clone> BackoffBuilder for ResetTimerBackoffBuilder<B> {
+    type Backoff = ResetTimerBackoff<ResettableBackoffWrapper<B>>;
 
     fn build(self) -> Self::Backoff {
         ResetTimerBackoff {
             inner_backoff: ResettableBackoffWrapper::new(self.inner_backoff_builder),
-            clock: self.clock,
             reset_duration: self.reset_duration,
             last_backoff: None,
         }
     }
 }
 
+
+/// Wraps a [`Backoff`] and resets it after a fixed duration of inactivity has elapsed.
+///
 /// Constructed by [`ResetTimerBackoffBuilder`].
 #[derive(Debug)]
-pub struct ResetTimerBackoff<B, C = TokioClock> {
+pub struct ResetTimerBackoff<B> {
     inner_backoff: B,
-    clock: C,
     reset_duration: Duration,
     last_backoff: Option<Instant>,
 }
 
 // impl Backoff, which is now effectively an alias for Iterator<Item = Duration>
-impl<B: ResettableBackoff, C: Clock> Iterator for ResetTimerBackoff<B, C> {
+impl<B: ResettableBackoff> Iterator for ResetTimerBackoff<B> {
     type Item = Duration;
 
     fn next(&mut self) -> Option<Duration> {
+        let now = tokio::time::Instant::now().into_std();
         if let Some(last_backoff) = self.last_backoff {
-            if self.clock.now() > last_backoff + self.reset_duration {
+            if now > last_backoff + self.reset_duration {
                 tracing::debug!(
                     ?last_backoff,
                     reset_duration = ?self.reset_duration,
@@ -78,7 +59,7 @@ impl<B: ResettableBackoff, C: Clock> Iterator for ResetTimerBackoff<B, C> {
                 self.inner_backoff.reset();
             }
         }
-        self.last_backoff = Some(self.clock.now());
+        self.last_backoff = Some(now);
         self.inner_backoff.next()
     }
 }
@@ -88,19 +69,15 @@ mod tests {
     use backon::BackoffBuilder;
     use tokio::time::advance;
 
-    use crate::utils::{
-        backoff_reset_timer::TokioClock, stream_backoff::tests::LinearBackoffBuilder,
-        ResetTimerBackoffBuilder,
-    };
+    use crate::utils::{stream_backoff::tests::LinearBackoffBuilder, ResetTimerBackoffBuilder};
     use std::time::Duration;
 
     #[tokio::test]
     async fn should_reset_when_timer_expires() {
         tokio::time::pause();
-        let mut backoff = ResetTimerBackoffBuilder::new_with_custom_clock(
+        let mut backoff = ResetTimerBackoffBuilder::new(
             LinearBackoffBuilder::new(Duration::from_secs(2)),
             Duration::from_secs(60),
-            TokioClock,
         )
         .build();
         assert_eq!(backoff.next(), Some(Duration::from_secs(2)));
