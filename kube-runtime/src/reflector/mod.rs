3b2b352b359a15f25c95c10bc396e1963a71efaa
clux <sszynrae@gmail.com>
2025-02-05 15:04:30 +0000

    fmt
    Signed-off-by: clux <sszynrae@gmail.com>

--- kube-runtime/src/reflector/mod.rs
+++ kube-runtime/src/reflector/mod.rs
@@ -12,8 +12,7 @@ use crate::watcher;
 use async_stream::stream;
 use futures::{Stream, StreamExt};
 use std::hash::Hash;
-#[cfg(feature = "unstable-runtime-subscribe")]
-pub use store::store_shared;
+#[cfg(feature = "unstable-runtime-subscribe")] pub use store::store_shared;
 pub use store::{store, Store};
 
 /// Cache objects from a [`watcher()`] stream into a local [`Store`]
