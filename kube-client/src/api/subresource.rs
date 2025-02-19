kube-rs/kube-rs

Exec can return stdout data even after stdin is closed. https://github.com/kube-rs/kube/issues/1689
Signed-off-by: Eric Webster <esw@amazon.com>

kube-client/src/api/subresource.rs
@@ -510,7 +510,7 @@ where
     pub async fn attach(&self, name: &str, ap: &AttachParams) -> Result<AttachedProcess> {
         let mut req = self.request.attach(name, ap).map_err(Error::BuildRequest)?;
         req.extensions_mut().insert("attach");
-        let stream = self.client.connect(req).await?;
+        let stream = self.client.connect_exec(req).await?;
         Ok(AttachedProcess::new(stream, ap))
     }
 }
@@ -565,7 +565,7 @@ where
             .exec(name, command, ap)
             .map_err(Error::BuildRequest)?;
         req.extensions_mut().insert("exec");
-        let stream = self.client.connect(req).await?;
+        let stream = self.client.connect_exec(req).await?;
         Ok(AttachedProcess::new(stream, ap))
     }
 }
@@ -606,7 +606,7 @@ where
             .request
             .portforward(name, ports)
             .map_err(Error::BuildRequest)?;
-        let stream = self.client.connect(req).await?;
-        Ok(Portforwarder::new(stream, ports))
+        let connection = self.client.connect_portforward(req).await?;
+        Ok(Portforwarder::new(connection.into_stream(), ports))
     }
 }

kube-client/src/client/kubelet_debug.rs
@@ -31,7 +31,7 @@ impl Client {
         let mut req =
             Request::kubelet_node_attach(kubelet_params, container, ap).map_err(Error::BuildRequest)?;
         req.extensions_mut().insert("kubelet_node_attach");
-        let stream = self.connect(req).await?;
+        let stream = self.connect_exec(req).await?;
         Ok(AttachedProcess::new(stream, ap))
     }
 
@@ -53,7 +53,7 @@ impl Client {
         let mut req = Request::kubelet_node_exec(kubelet_params, container, command, ap)
             .map_err(Error::BuildRequest)?;
         req.extensions_mut().insert("kubelet_node_exec");
-        let stream = self.connect(req).await?;
+        let stream = self.connect_exec(req).await?;
         Ok(AttachedProcess::new(stream, ap))
     }
 
@@ -69,8 +69,8 @@ impl Client {
         let mut req =
             Request::kubelet_node_portforward(kubelet_params, ports).map_err(Error::BuildRequest)?;
         req.extensions_mut().insert("kubelet_node_portforward");
-        let stream = self.connect(req).await?;
-        Ok(Portforwarder::new(stream, ports))
+        let connection = self.connect_portforward(req).await?;
+        Ok(Portforwarder::new(connection.into_stream(), ports))
     }
 
     /// Stream logs directly from node

kube-rs/kube

kube-client/src/api/subresource.rs
@@ -606,7 +606,7 @@ where
             .request
             .portforward(name, ports)
             .map_err(Error::BuildRequest)?;
-        let stream = self.client.connect(req).await?;
-        Ok(Portforwarder::new(stream, ports))
+        let connection = self.client.connect(req).await?;
+        Ok(Portforwarder::new(connection.into_stream(), ports))
     }
 }

kube-client/src/client/kubelet_debug.rs
@@ -69,8 +69,8 @@ impl Client {
         let mut req =
             Request::kubelet_node_portforward(kubelet_params, ports).map_err(Error::BuildRequest)?;
         req.extensions_mut().insert("kubelet_node_portforward");
-        let stream = self.connect(req).await?;
-        Ok(Portforwarder::new(stream, ports))
+        let connection = self.connect(req).await?;
+        Ok(Portforwarder::new(connection.into_stream(), ports))
     }
 
     /// Stream logs directly from node