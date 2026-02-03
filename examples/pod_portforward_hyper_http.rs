3537ea6a081eb942bc3653cb2ecea2b60d67a651
tottoto <tottotodev@gmail.com>
2026-02-04 06:03:10 +0900

    refactor request and response construct
    Signed-off-by: tottoto <tottotodev@gmail.com>

--- examples/pod_portforward_hyper_http.rs
+++ examples/pod_portforward_hyper_http.rs
@@ -47,11 +47,9 @@ async fn main() -> anyhow::Result<()> {
         }
     });
 
-    let http_req = Request::builder()
-        .uri("/")
+    let http_req = Request::get("/")
         .header("Connection", "close")
         .header("Host", "127.0.0.1")
-        .method("GET")
         .body(http_body_util::Empty::<Bytes>::new())
         .unwrap();
 
