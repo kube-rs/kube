46aa7c8be3665cabd42406ce67585d77e45d1c8b
github-action-benchmark <github@users.noreply.github.com>
2026-02-24 13:53:55 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1320643f8ce7f8189e03496ff1329d678d76224c

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771548270051,
+  "lastUpdate": 1771941235533,
   "repoUrl": "https://github.com/kube-rs/kube",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,105 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kube-rs/kube/commit/58cf5a6d762d49e101c304ec060ef60bec2769c3"
         },
         "date": 1771548268859,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "init_listwatch - peak_bytes",
+            "value": 55194619,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - total_allocated",
+            "value": 76715088,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - alloc_count",
+            "value": 578023,
+            "unit": "allocations"
+          },
+          {
+            "name": "steady_state - peak_bytes",
+            "value": 71381202,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - total_allocated",
+            "value": 109519220,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - alloc_count",
+            "value": 799021,
+            "unit": "allocations"
+          },
+          {
+            "name": "relist - peak_bytes",
+            "value": 99797302,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - total_allocated",
+            "value": 174518628,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - alloc_count",
+            "value": 1189035,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_without_modify - peak_bytes",
+            "value": 141298836,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - total_allocated",
+            "value": 205865000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - alloc_count",
+            "value": 1298020,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_with_modify - peak_bytes",
+            "value": 134853452,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - total_allocated",
+            "value": 162895000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - alloc_count",
+            "value": 1058021,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "10092581+NickLarsenNZ@users.noreply.github.com",
+            "name": "Nick",
+            "username": "NickLarsenNZ"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1320643f8ce7f8189e03496ff1329d678d76224c",
+          "message": "fix: Produce valid CRDs containing flattened untagged enums (#1942)\n\n* test(kube-derive): Add failing test for a flattened and untagged enum\n\nNote: This comes from #1839, with some modifications (an extra field to the B variant, and change comments to indicate untagged variant descriptions should not leak into fields).\n\nCo-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.tech>\n\nSigned-off-by: Nick Larsen <nick.larsen@stackable.tech>\n\n* fix(kube-core): Remove lingering type and description fields from variants without fields\n\nSigned-off-by: Nick Larsen <nick.larsen@stackable.tech>\n\n* fix(kube-core/schema): Only push variant descriptions into properties for oneOf\n\nNote: variant descriptions are meaningless for untagged enums - they don't correspond to the fields inside struct variants.\n\nSigned-off-by: Nick Larsen <nick.larsen@stackable.tech>\n\n* chore(kube-core/schema): Adjust comment and move higher\n\nSigned-off-by: Nick Larsen <nick.larsen@stackable.tech>\n\n---------\n\nSigned-off-by: Nick Larsen <nick.larsen@stackable.tech>",
+          "timestamp": "2026-02-24T13:51:36Z",
+          "tree_id": "9b23e92a22ef2d4d35445d035939d601457892a1",
+          "url": "https://github.com/kube-rs/kube/commit/1320643f8ce7f8189e03496ff1329d678d76224c"
+        },
+        "date": 1771941234322,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
