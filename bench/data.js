80017bac4231100e7e9a2f162764d2b959c734c8
github-action-benchmark <github@users.noreply.github.com>
2026-05-04 09:21:05 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 21b0f61aa9e72cc24e3a90073a78dfb31abccebd

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777650250370,
+  "lastUpdate": 1777886465146,
   "repoUrl": "https://github.com/kube-rs/kube",
   "entries": {
     "Benchmark": [
@@ -1310,6 +1310,105 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kube-rs/kube/commit/e7060a8713aabaa209cb004f49f1ab4d13ff2226"
         },
         "date": 1777650249370,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "init_listwatch - peak_bytes",
+            "value": 55194619,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - total_allocated",
+            "value": 76715088,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - alloc_count",
+            "value": 578023,
+            "unit": "allocations"
+          },
+          {
+            "name": "steady_state - peak_bytes",
+            "value": 71381202,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - total_allocated",
+            "value": 109519220,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - alloc_count",
+            "value": 799021,
+            "unit": "allocations"
+          },
+          {
+            "name": "relist - peak_bytes",
+            "value": 99797302,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - total_allocated",
+            "value": 174518628,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - alloc_count",
+            "value": 1189035,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_without_modify - peak_bytes",
+            "value": 141298836,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - total_allocated",
+            "value": 205865000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - alloc_count",
+            "value": 1298020,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_with_modify - peak_bytes",
+            "value": 134853452,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - total_allocated",
+            "value": 162895000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - alloc_count",
+            "value": 1058021,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@goenning.net",
+            "name": "Guilherme Oenning",
+            "username": "goenning"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "21b0f61aa9e72cc24e3a90073a78dfb31abccebd",
+          "message": "Remove silent error when client-key/client-certificate is malformed (#1966)\n\n* fix silent err\n\nSigned-off-by: goenning <me@goenning.net>\n\n* remove unused\n\nSigned-off-by: goenning <me@goenning.net>\n\n* fix unit tests\n\nSigned-off-by: goenning <me@goenning.net>\n\n* fix openssl compile error\n\nSigned-off-by: goenning <me@goenning.net>\n\n---------\n\nSigned-off-by: goenning <me@goenning.net>\nCo-authored-by: Eirik A <sszynrae@gmail.com>",
+          "timestamp": "2026-05-04T10:20:01+01:00",
+          "tree_id": "9194d705dc9ac7c910f0bc857e232a4bb0e2a8bb",
+          "url": "https://github.com/kube-rs/kube/commit/21b0f61aa9e72cc24e3a90073a78dfb31abccebd"
+        },
+        "date": 1777886464171,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
