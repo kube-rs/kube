4e3660af659ef99a12a6dcbda409b5ccee2f495b
github-action-benchmark <github@users.noreply.github.com>
2026-06-15 14:03:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3dd76bc096be66bb6d90dfd269fddc6ff1e81a2c

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781360725121,
+  "lastUpdate": 1781532196831,
   "repoUrl": "https://github.com/kube-rs/kube",
   "entries": {
     "Benchmark": [
@@ -3191,6 +3191,105 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kube-rs/kube/commit/c261d787af68af60616bcde3ffac5d2ee51297f9"
         },
         "date": 1781360723444,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "init_listwatch - peak_bytes",
+            "value": 55194619,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - total_allocated",
+            "value": 76715088,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - alloc_count",
+            "value": 578023,
+            "unit": "allocations"
+          },
+          {
+            "name": "steady_state - peak_bytes",
+            "value": 71381202,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - total_allocated",
+            "value": 109519220,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - alloc_count",
+            "value": 799021,
+            "unit": "allocations"
+          },
+          {
+            "name": "relist - peak_bytes",
+            "value": 99797302,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - total_allocated",
+            "value": 174518628,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - alloc_count",
+            "value": 1189035,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_without_modify - peak_bytes",
+            "value": 141298836,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - total_allocated",
+            "value": 205865000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - alloc_count",
+            "value": 1298020,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_with_modify - peak_bytes",
+            "value": 134853452,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - total_allocated",
+            "value": 162895000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - alloc_count",
+            "value": 1058021,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sszynrae@gmail.com",
+            "name": "Eirik A",
+            "username": "clux"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3dd76bc096be66bb6d90dfd269fddc6ff1e81a2c",
+          "message": "bump k8s-openapi to 0.28 (#2009)\n\n* bump k8s-openapi to 0.28\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* actually bump\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* minimal version syntax\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* minor breaking changes in TokenRequest / TokenReview\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* bump more refs\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* find one more version failing to bump\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n---------\n\nSigned-off-by: clux <sszynrae@gmail.com>",
+          "timestamp": "2026-06-15T15:02:02+01:00",
+          "tree_id": "863669a1badd0aa561e2d58895b33db77880faee",
+          "url": "https://github.com/kube-rs/kube/commit/3dd76bc096be66bb6d90dfd269fddc6ff1e81a2c"
+        },
+        "date": 1781532195722,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
