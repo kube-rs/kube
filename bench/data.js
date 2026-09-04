66afd027834cd77807c515ed808681c2de90051e
github-action-benchmark <github@users.noreply.github.com>
2026-09-04 01:34:36 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for bb2e548219fc30824c96512bc514c20276ba2357

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788009042585,
+  "lastUpdate": 1788485676971,
   "repoUrl": "https://github.com/kube-rs/kube",
   "entries": {
     "Benchmark": [
@@ -5567,6 +5567,105 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kube-rs/kube/commit/7a4641d4cc2f693b2dee97b9fc15fadb96d7f62e"
         },
         "date": 1788009041443,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "init_listwatch - peak_bytes",
+            "value": 55194619,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - total_allocated",
+            "value": 76715088,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - alloc_count",
+            "value": 578023,
+            "unit": "allocations"
+          },
+          {
+            "name": "steady_state - peak_bytes",
+            "value": 71381202,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - total_allocated",
+            "value": 109519220,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - alloc_count",
+            "value": 799021,
+            "unit": "allocations"
+          },
+          {
+            "name": "relist - peak_bytes",
+            "value": 99797302,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - total_allocated",
+            "value": 174518628,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - alloc_count",
+            "value": 1189035,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_without_modify - peak_bytes",
+            "value": 141298836,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - total_allocated",
+            "value": 205865000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - alloc_count",
+            "value": 1298020,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_with_modify - peak_bytes",
+            "value": 134853452,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - total_allocated",
+            "value": 162895000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - alloc_count",
+            "value": 1058021,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aregak2005@gmail.com",
+            "name": "MintSoup",
+            "username": "MintSoup"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bb2e548219fc30824c96512bc514c20276ba2357",
+          "message": "Expose concrete types in functions that return `impl AsyncRead/AsyncWrite` (#2059)\n\nExpose concrete types in functions that return impl AsyncRead/AsyncWrite\n\nSigned-off-by: MintSoup <aregak2005@gmail.com>",
+          "timestamp": "2026-09-04T02:32:54+01:00",
+          "tree_id": "c5179faa17a7d22bf2eb040e2ace15fef40cabee",
+          "url": "https://github.com/kube-rs/kube/commit/bb2e548219fc30824c96512bc514c20276ba2357"
+        },
+        "date": 1788485675393,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
