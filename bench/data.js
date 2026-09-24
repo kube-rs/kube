aeaf1cfc36033302e7eee7d8786c91543d372d16
github-action-benchmark <github@users.noreply.github.com>
2026-09-24 08:12:56 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 020ffda1707c6b2ec3dc4e82373b5e6d17446417

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790176838250,
+  "lastUpdate": 1790237576197,
   "repoUrl": "https://github.com/kube-rs/kube",
   "entries": {
     "Benchmark": [
@@ -6557,6 +6557,105 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kube-rs/kube/commit/9a4d12c8909d17a2f30da7b539e4ce6baae6dee1"
         },
         "date": 1790176837077,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "init_listwatch - peak_bytes",
+            "value": 55194619,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - total_allocated",
+            "value": 76715088,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - alloc_count",
+            "value": 578023,
+            "unit": "allocations"
+          },
+          {
+            "name": "steady_state - peak_bytes",
+            "value": 71381202,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - total_allocated",
+            "value": 109519220,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - alloc_count",
+            "value": 799021,
+            "unit": "allocations"
+          },
+          {
+            "name": "relist - peak_bytes",
+            "value": 99797302,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - total_allocated",
+            "value": 174518628,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - alloc_count",
+            "value": 1189035,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_without_modify - peak_bytes",
+            "value": 141298836,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - total_allocated",
+            "value": 205865000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - alloc_count",
+            "value": 1298020,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_with_modify - peak_bytes",
+            "value": 134853452,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - total_allocated",
+            "value": 162895000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - alloc_count",
+            "value": 1058021,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sszynrae@gmail.com",
+            "name": "Eirik A",
+            "username": "clux"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "020ffda1707c6b2ec3dc4e82373b5e6d17446417",
+          "message": "fix minor cargo + clippy lints (#2087)\n\n* fix minor cargo lints\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* obivously needed\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* fmt\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* guess this is too ambitious\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n* ok this as also\n\nSigned-off-by: clux <sszynrae@gmail.com>\n\n---------\n\nSigned-off-by: clux <sszynrae@gmail.com>",
+          "timestamp": "2026-09-24T09:12:11+01:00",
+          "tree_id": "b10d88ebc4eeb6a7fb5d6b89dd5372836ecc9275",
+          "url": "https://github.com/kube-rs/kube/commit/020ffda1707c6b2ec3dc4e82373b5e6d17446417"
+        },
+        "date": 1790237574441,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
