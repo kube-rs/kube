bac6403b11333eaa0361dff84679aa5c1561caa3
clux <sszynrae@gmail.com>
2026-09-24 08:39:20 +0100

    fmt
    Signed-off-by: clux <sszynrae@gmail.com>

--- kube-core/src/cel/mod.rs
+++ kube-core/src/cel/mod.rs
@@ -13,8 +13,7 @@ use std::{collections::BTreeMap, str::FromStr};
 pub use kube_cel::*;
 
 use derive_more::From;
-#[cfg(feature = "schema")]
-use schemars::Schema;
+#[cfg(feature = "schema")] use schemars::Schema;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
