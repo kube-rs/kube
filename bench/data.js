d327cbf4de5a9540ae538bc1417dc81ee6aa2e50
github-action-benchmark <github@users.noreply.github.com>
2026-09-26 04:24:01 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 237386c1b9384f4e674a48178907d9cd4f11b0a0

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790237576197,
+  "lastUpdate": 1790396641030,
   "repoUrl": "https://github.com/kube-rs/kube",
   "entries": {
     "Benchmark": [
@@ -6656,6 +6656,105 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kube-rs/kube/commit/020ffda1707c6b2ec3dc4e82373b5e6d17446417"
         },
         "date": 1790237574441,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "init_listwatch - peak_bytes",
+            "value": 55194619,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - total_allocated",
+            "value": 76715088,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_listwatch - alloc_count",
+            "value": 578023,
+            "unit": "allocations"
+          },
+          {
+            "name": "steady_state - peak_bytes",
+            "value": 71381202,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - total_allocated",
+            "value": 109519220,
+            "unit": "bytes"
+          },
+          {
+            "name": "steady_state - alloc_count",
+            "value": 799021,
+            "unit": "allocations"
+          },
+          {
+            "name": "relist - peak_bytes",
+            "value": 99797302,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - total_allocated",
+            "value": 174518628,
+            "unit": "bytes"
+          },
+          {
+            "name": "relist - alloc_count",
+            "value": 1189035,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_without_modify - peak_bytes",
+            "value": 141298836,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - total_allocated",
+            "value": 205865000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_without_modify - alloc_count",
+            "value": 1298020,
+            "unit": "allocations"
+          },
+          {
+            "name": "init_with_modify - peak_bytes",
+            "value": 134853452,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - total_allocated",
+            "value": 162895000,
+            "unit": "bytes"
+          },
+          {
+            "name": "init_with_modify - alloc_count",
+            "value": 1058021,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jesse@szwedko.me",
+            "name": "Jesse Szwedko",
+            "username": "jszwedko"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "237386c1b9384f4e674a48178907d9cd4f11b0a0",
+          "message": "Update serde-saphyr requirement from 0.0.29 to 1.3.0 (#2086)\n\nUpdate serde-saphyr requirement from 0.0.29 to 1.0.0\n\nUpdating serde-saphry to relax the constraint on smallvec.\n\nNote that serde_saphyr::Error is exposed in kube-client's public API\n(KubeconfigError::Parse and auth::Error::AuthExecParse).\n\nSigned-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>",
+          "timestamp": "2026-09-26T05:22:47+01:00",
+          "tree_id": "533b09c86414f04375b3625948c22eb0b9c26276",
+          "url": "https://github.com/kube-rs/kube/commit/237386c1b9384f4e674a48178907d9cd4f11b0a0"
+        },
+        "date": 1790396639913,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
